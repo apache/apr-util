f1ec51a80333089d860908f821dd492fc8c322f3
Ben Laurie <ben@apache.org>
1999-07-06 21:32:08 +0000

    More hooky stuff.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@57763 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_hooks.h
+++ include/ap_hooks.h
@@ -1,6 +1,10 @@
+#ifndef APACHE_AP_HOOKS_H
+#define APACHE_AP_HOOKS_H
+
 #define DECLARE_HOOK(ret,name,args) \
 typedef ret HOOK_##name args; \
-void hook_##name(HOOK_##name *pf); \
+void ap_hook_##name(HOOK_##name *pf); \
+ret ap_run_##name args; \
 typedef struct _LINK_##name \
     { \
     HOOK_##name *pFunc; \
@@ -13,15 +17,15 @@ static struct { members } _hooks;
 #define HOOK_LINK(name) \
     LINK_##name *link_##name;
 
-#define IMPLEMENT_HOOK_BASE(ret,rv_decl,sv,rv,name,args,args2,run_all,terminate) \
-void hook_##name(HOOK_##name *pf) \
+#define IMPLEMENT_HOOK_BASE(ret,rv_decl,sv,rv,name,args,args2,run_all,term1,term2,rv_final) \
+void ap_hook_##name(HOOK_##name *pf) \
     { \
     LINK_##name *pHook=ap_palloc(g_pHookPool,sizeof(LINK_##name)); \
     pHook->pNext=_hooks.link_##name; \
     pHook->pFunc=pf; \
     _hooks.link_##name=pHook; \
     } \
-ret run_##name args \
+ret ap_run_##name args \
     { \
     LINK_##name *pHook; \
     rv_decl \
@@ -30,15 +34,17 @@ ret run_##name args \
 	{ \
 	sv pHook->pFunc args2; \
 \
-	if(!run_all terminate) \
+	if(term1 && (!run_all || term2)) \
 	    return rv; \
 	} \
-    return rv; \
+    return rv_final; \
     }
 
-#define IMPLEMENT_HOOK(ret,name,args,args2,run_all,finish) \
-	IMPLEMENT_HOOK_BASE(ret,ret r;,r=,r,name,args,args2,run_all,&& r == finish)
+#define IMPLEMENT_HOOK(ret,name,args,args2,run_all,ok,decline) \
+	IMPLEMENT_HOOK_BASE(ret,ret r_;,r_=,r_,name,args,args2,run_all,r_ != decline,r_ != ok,run_all ? ok : decline)
 #define IMPLEMENT_VOID_HOOK(name,args,args2,run_all) \
-	IMPLEMENT_HOOK_BASE(void,,,,name,args,args2,run_all,)
+	IMPLEMENT_HOOK_BASE(void,,,,name,args,args2,run_all,1,0,)
 
 extern pool *g_pHookPool;
+
+#endif /* ndef(AP_HOOKS_H) */
