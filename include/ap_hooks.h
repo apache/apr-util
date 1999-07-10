6cc8f6caa1e4a1e04bda40c5c12fdd2f233602d1
Ben Laurie <ben@apache.org>
1999-07-10 18:58:17 +0000

    Use arrays for hooks.
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@57765 13f79535-47bb-0310-9956-ffa450edef68

--- include/ap_hooks.h
+++ include/ap_hooks.h
@@ -10,33 +10,38 @@ ret ap_run_##name args; \
 typedef struct _LINK_##name \
     { \
     HOOK_##name *pFunc; \
-    struct _LINK_##name *pNext; \
     } LINK_##name;
 
 #define HOOK_STRUCT(members) \
 static struct { members } _hooks;
 
 #define HOOK_LINK(name) \
-    LINK_##name *link_##name;
+    array_header *link_##name;
 
 #define IMPLEMENT_HOOK_BASE(ret,rv_decl,sv,rv,name,args,args2,run_all,term1,term2,rv_final) \
 void ap_hook_##name(HOOK_##name *pf) \
     { \
-    LINK_##name *pHook=ap_palloc(g_pHookPool,sizeof(LINK_##name)); \
-    pHook->pNext=_hooks.link_##name; \
+    LINK_##name *pHook; \
+    if(!_hooks.link_##name) \
+	_hooks.link_##name=ap_make_array(g_pHookPool,1,sizeof(LINK_##name)); \
+    pHook=ap_push_array(_hooks.link_##name); \
     pHook->pFunc=pf; \
-    _hooks.link_##name=pHook; \
     if(g_bDebugHooks) \
 	puts("  Hooked " #name); \
     } \
 ret ap_run_##name args \
     { \
     LINK_##name *pHook; \
+    int n; \
     rv_decl \
 \
-    for(pHook=_hooks.link_##name ; pHook ; pHook=pHook->pNext) \
+    if(!_hooks.link_##name) \
+	return rv_final; \
+\
+    pHook=(LINK_##name *)_hooks.link_##name->elts; \
+    for(n=0 ; n < _hooks.link_##name->nelts ; ++n) \
 	{ \
-	sv pHook->pFunc args2; \
+	sv pHook[n].pFunc args2; \
 \
 	if(term1 && (!run_all || term2)) \
 	    return rv; \
