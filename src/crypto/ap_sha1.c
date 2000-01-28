7993c8a3c396068bcf9a561a5b35a6bc950e71d0
Ryan Bloom <rbb@apache.org>
2000-01-28 18:00:35 +0000

    Include ap_config.h before httpd.h, this ensures that AP_USE_HSREGEX is defined correctly in all C files.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@57788 13f79535-47bb-0310-9956-ffa450edef68

--- src/crypto/ap_sha1.c
+++ src/crypto/ap_sha1.c
@@ -84,13 +84,13 @@
 
 #include <string.h>
 
+#include "ap_config.h"
 #include "httpd.h"
 #include "ap_sha1.h"
 #include "ap.h"
 #ifdef CHARSET_EBCDIC
 #include "ebcdic.h"
 #endif /*CHARSET_EBCDIC*/
-#include "ap_config.h"
 
 /* a bit faster & bigger, if defined */
 #define UNROLL_LOOPS
