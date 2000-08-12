1fc3618518ee0e00ed42f039d1b3fd569263a5e7
Ryan Bloom <rbb@apache.org>
2000-08-12 18:45:32 +0000

    Initial Filtering code.  This uses a bucket brigade scheme to allow modules to add and modify data while processing a request.  The docs still need to be updated, and a simple html page needs to be created explaining all of this.
    The only filter currently in the code is the core filter.  This filter
    takes a bucket brigade and writes it to the network through the buff
    structure.  In time, the buff will go away completely.
    
    More filters will need to be written.
    Submitted by:	The Apache Community
    Reviewed by:	The Apache Community
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@57827 13f79535-47bb-0310-9956-ffa450edef68

---
+++ buckets/apr_buckets_eos.c
@@ -0,0 +1,86 @@
+/* ====================================================================
+ * Copyright (c) 1996-1999 The Apache Group.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the Apache Group
+ *    for use in the Apache HTTP server project (http://www.apache.org/)."
+ *
+ * 4. The names "Apache Server" and "Apache Group" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For written permission, please contact
+ *    apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the Apache Group
+ *    for use in the Apache HTTP server project (http://www.apache.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE APACHE GROUP ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE APACHE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Group and was originally based
+ * on public domain software written at the National Center for
+ * Supercomputing Applications, University of Illinois, Urbana-Champaign.
+ * For more information on the Apache Group and the Apache HTTP server
+ * project, please see <http://www.apache.org/>.
+ *
+ */
+
+#include "apr_private.h"
+#include "ap_buckets.h"
+#include <stdlib.h>
+
+static apr_status_t eos_get_str(ap_bucket *e, const char **str, 
+                                apr_ssize_t *len, int block)
+{
+    *str = NULL;
+    *len = AP_END_OF_BRIGADE;
+    return AP_END_OF_BRIGADE;
+}
+
+APR_EXPORT(ap_bucket *) ap_bucket_eos_create(void)
+{
+    ap_bucket *newbuf;
+
+    newbuf            = calloc(1, sizeof(*newbuf));
+    newbuf->length    = AP_END_OF_BRIGADE;
+
+    newbuf->type      = AP_BUCKET_EOS;
+    newbuf->read      = eos_get_str;
+    newbuf->setaside  = NULL;
+    newbuf->split     = NULL;
+    newbuf->destroy   = NULL;
+    newbuf->data      = NULL;
+    
+    return newbuf;
+}
+
