ffcc892ed27e3430cb1a29eeca3e8e0d71e6cd90
Greg Stein <gstein@apache.org>
2000-12-02 16:13:50 +0000

    initial population of the APRUTIL module.
    *) add configure/build system
    *) add an initial directory layout (from email proposal)
    *) add the SDBM files as a quick test of the build
       [ these came from apache-2.0/src/lib/sdbm; no tag ]
    *) add an empty STATUS file for tracking the project
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@57932 13f79535-47bb-0310-9956-ffa450edef68

---
+++ buildconf.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Get a copy of libtool's libtool.m4 and copy it into our aclocal.m4
+#
+# If we ever have our own .m4 files, then we will want to concatenate
+# them altogether into aclocal.m4
+#
+### we may need to get smarter with these two lines
+ltpath=`which libtoolize`
+ltpath=`dirname $ltpath`
+ltfile=`cd $ltpath/../share/aclocal ; pwd`/libtool.m4
+echo "Incorporating $ltfile into aclocal.m4 ..."
+cat $ltfile > aclocal.m4
+
+#
+# Create the libtool helper files
+#
+# Note: we always replace the files, and we copy (rather than link) them.
+#
+echo "Copying libtool helper files ..."
+$ltpath/libtoolize --force --copy
+
+#
+# Generate the autoconf header (include/apu_config.h) and ./configure
+#
+echo "Creating include/apu_config.h ..."
+autoheader
+
+echo "Creating configure ..."
+### do some work to toss config.cache?
+autoconf
