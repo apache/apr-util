1ed7593ee4dc478156c562efa1ffde5ebb300ad8
Ryan Bloom <rbb@apache.org>
2000-12-19 17:05:45 +0000

    Force all Apache functions to be linked into the executable, whether they are used or not.  This uses the same mechanism that is used for APR and APR-util.  This may not be the correct solution, but it works, and that is what I really care about.  This also renames CHARSET_EBCDIC to AP_CHARSET_EBCDIC.  This is for namespace correctness, but it also makes the exports script a bit easier.
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@57998 13f79535-47bb-0310-9956-ffa450edef68

--- crypto/ap_sha1.c
+++ crypto/ap_sha1.c
@@ -86,9 +86,9 @@
 #include "ap_base64.h"
 #include "apr_strings.h"
 #include "apr_lib.h"
-#ifdef CHARSET_EBCDIC
+#ifdef AP_CHARSET_EBCDIC
 #include "apr_xlate.h"
-#endif /*CHARSET_EBCDIC*/
+#endif /*AP_CHARSET_EBCDIC*/
 #include <string.h>
 
 /* a bit faster & bigger, if defined */
@@ -119,7 +119,7 @@
 
 #define SHA_BLOCKSIZE           64
 
-#ifdef CHARSET_EBCDIC
+#ifdef AP_CHARSET_EBCDIC
 static apr_xlate_t *ebcdic2ascii_xlate;
 
 APR_DECLARE(apr_status_t) ap_SHA1InitEBCDIC(apr_xlate_t *x)
@@ -299,7 +299,7 @@ APR_DECLARE(void) ap_SHA1Update_binary(AP_SHA1_CTX *sha_info,
 APR_DECLARE(void) ap_SHA1Update(AP_SHA1_CTX *sha_info, const char *buf,
                               unsigned int count)
 {
-#ifdef CHARSET_EBCDIC
+#ifdef AP_CHARSET_EBCDIC
     int i;
     const AP_BYTE *buffer = (const AP_BYTE *) buf;
     apr_size_t inbytes_left, outbytes_left;
