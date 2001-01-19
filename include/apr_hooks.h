5e1095512c3e5a04efe817b3ad66315875229970
William A. Rowe Jr <wrowe@apache.org>
2001-01-19 07:01:27 +0000

    Changes at-a-glance for the symbol rename.  Files removed now per   comments that the two copies in the tree break the build.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58049 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_hooks.h
+++ include/apr_hooks.h
@@ -52,8 +52,8 @@
  * <http://www.apache.org/>.
  */
 
-#ifndef APACHE_AP_HOOKS_H
-#define APACHE_AP_HOOKS_H
+#ifndef APR_HOOKS_H
+#define APR_HOOKS_H
 
 #include "apu.h"
 /* For apr_array_header_t */
@@ -67,69 +67,71 @@ extern "C" {
  * @package Apache hooks functions
  */
 
-#define AP_DECLARE_EXTERNAL_HOOK(link,ret,name,args) \
-typedef ret HOOK_##name args; \
-link##_DECLARE(void) ap_hook_##name(HOOK_##name *pf, const char* const* aszPre, \
-                                    const char * const *aszSucc, int nOrder); \
-link##_DECLARE(ret) ap_run_##name args; \
-typedef struct _LINK_##name \
+#define APR_DECLARE_EXTERNAL_HOOK(ns,link,ret,name,args) \
+typedef ret ns##_HOOK_##name##_t args; \
+link##_DECLARE(void) ns##_hook_##name(ns##_HOOK_##name##_t *pf, \
+                                      const char* const* aszPre, \
+                                      const char * const *aszSucc, int nOrder); \
+link##_DECLARE(ret) ns##_run_##name args; \
+typedef struct ns##_LINK_##name##_t \
     { \
-    HOOK_##name *pFunc; \
+    ns##_HOOK_##name##_t *pFunc; \
     const char *szName; \
     const char * const *aszPredecessors; \
     const char * const *aszSuccessors; \
     int nOrder; \
-    } LINK_##name;
+    } ns##_LINK_##name##_t;
 
-#define AP_HOOK_STRUCT(members) \
+#define APR_HOOK_STRUCT(members) \
 static struct { members } _hooks;
 
-#define AP_HOOK_LINK(name) \
+#define APR_HOOK_LINK(name) \
     apr_array_header_t *link_##name;
 
-#define AP_IMPLEMENT_EXTERNAL_HOOK_BASE(link,name) \
-link##_DECLARE(void) ap_hook_##name(HOOK_##name *pf,const char * const *aszPre, \
-                                    const char * const *aszSucc,int nOrder) \
+#define APR_IMPLEMENT_EXTERNAL_HOOK_BASE(ns,link,name) \
+link##_DECLARE(void) ns##_hook_##name(ns##_HOOK_##name##_t *pf,const char * const *aszPre, \
+                                      const char * const *aszSucc,int nOrder) \
     { \
-    LINK_##name *pHook; \
+    ns##_LINK_##name##_t *pHook; \
     if(!_hooks.link_##name) \
 	{ \
-	_hooks.link_##name=apr_make_array(ap_global_hook_pool,1,sizeof(LINK_##name)); \
-	ap_hook_sort_register(#name,&_hooks.link_##name); \
+	_hooks.link_##name=apr_make_array(apr_global_hook_pool,1,sizeof(ns##_LINK_##name##_t)); \
+	apr_hook_sort_register(#name,&_hooks.link_##name); \
 	} \
     pHook=apr_push_array(_hooks.link_##name); \
     pHook->pFunc=pf; \
     pHook->aszPredecessors=aszPre; \
     pHook->aszSuccessors=aszSucc; \
     pHook->nOrder=nOrder; \
-    pHook->szName=ap_current_hooking_module; \
-    if(ap_debug_module_hooks) \
-	ap_show_hook(#name,aszPre,aszSucc); \
+    pHook->szName=apr_debug_module_name; \
+    if(apr_debug_module_hooks) \
+	apr_show_hook(#name,aszPre,aszSucc); \
     }
 
 /**
  * Implement a hook that has no return code, and therefore runs all of the
  * registered functions
+ * @param ns The namespace prefix of the hook functions
  * @param link The linkage declaration prefix of the hook
  * @param name The name of the hook
  * @param args_decl The declaration of the arguments for the hook
  * @param args_used The names for the arguments for the hook
- * @deffunc void AP_IMPLEMENT_EXTERNAL_HOOK_VOID(link, name, args_decl, args_use)
+ * @deffunc void APR_IMPLEMENT_EXTERNAL_HOOK_VOID(ns, link, name, args_decl, args_use)
  * @tip The link prefix FOO corresponds to FOO_DECLARE() macros, which
  * provide export linkage from the module that IMPLEMENTs the hook, and
  * import linkage from external modules that link to the hook's module.
  */
-#define AP_IMPLEMENT_EXTERNAL_HOOK_VOID(link,name,args_decl,args_use) \
-AP_IMPLEMENT_EXTERNAL_HOOK_BASE(link,name) \
-link##_DECLARE(void) ap_run_##name args_decl \
+#define APR_IMPLEMENT_EXTERNAL_HOOK_VOID(ns,link,name,args_decl,args_use) \
+APR_IMPLEMENT_EXTERNAL_HOOK_BASE(ns,link,name) \
+link##_DECLARE(void) ns##_run_##name args_decl \
     { \
-    LINK_##name *pHook; \
+    ns##_LINK_##name##_t *pHook; \
     int n; \
 \
     if(!_hooks.link_##name) \
 	return; \
 \
-    pHook=(LINK_##name *)_hooks.link_##name->elts; \
+    pHook=(ns##_LINK_##name##_t *)_hooks.link_##name->elts; \
     for(n=0 ; n < _hooks.link_##name->nelts ; ++n) \
 	pHook[n].pFunc args_use; \
     }
@@ -140,27 +142,28 @@ link##_DECLARE(void) ap_run_##name args_decl \
 /**
  * Implement a hook that runs until one of the functions returns something
  * other than OK or DECLINE
+ * @param ns The namespace prefix of the hook functions
  * @param link The linkage declaration prefix of the hook
  * @param name The name of the hook
  * @param args_decl The declaration of the arguments for the hook
  * @param args_used The names for the arguments for the hook
- * @deffunc int AP_IMPLEMENT_EXTERNAL_HOOK_RUN_ALL(link, name, args_decl, args_use)
+ * @deffunc int APR_IMPLEMENT_EXTERNAL_HOOK_RUN_ALL(ns, link, name, args_decl, args_use)
  * @tip The link prefix FOO corresponds to FOO_DECLARE() macros, which
  * provide export linkage from the module that IMPLEMENTs the hook, and
  * import linkage from external modules that link to the hook's module.
  */
-#define AP_IMPLEMENT_EXTERNAL_HOOK_RUN_ALL(link,ret,name,args_decl,args_use,ok,decline) \
-AP_IMPLEMENT_EXTERNAL_HOOK_BASE(link,name) \
-link##_DECLARE(ret) ap_run_##name args_decl \
+#define APR_IMPLEMENT_EXTERNAL_HOOK_RUN_ALL(ns,link,ret,name,args_decl,args_use,ok,decline) \
+APR_IMPLEMENT_EXTERNAL_HOOK_BASE(ns,link,name) \
+link##_DECLARE(ret) ns##_run_##name args_decl \
     { \
-    LINK_##name *pHook; \
+    ns##_LINK_##name##_t *pHook; \
     int n; \
     ret rv; \
 \
     if(!_hooks.link_##name) \
 	return ok; \
 \
-    pHook=(LINK_##name *)_hooks.link_##name->elts; \
+    pHook=(ns##_LINK_##name##_t *)_hooks.link_##name->elts; \
     for(n=0 ; n < _hooks.link_##name->nelts ; ++n) \
 	{ \
 	rv=pHook[n].pFunc args_use; \
@@ -174,28 +177,29 @@ link##_DECLARE(ret) ap_run_##name args_decl \
 
 /**
  * Implement a hook that runs until the first function returns something
- * other than DECLINE
+ * other than the value of decline
+ * @param ns The namespace prefix of the hook functions
  * @param link The linkage declaration prefix of the hook
  * @param name The name of the hook
  * @param args_decl The declaration of the arguments for the hook
  * @param args_used The names for the arguments for the hook
- * @deffunc int AP_IMPLEMENT_HOOK_RUN_FIRST(link, name, args_decl, args_use)
+ * @deffunc int APR_IMPLEMENT_HOOK_RUN_FIRST(ns, link, name, args_decl, args_use, decline)
  * @tip The link prefix FOO corresponds to FOO_DECLARE() macros, which
  * provide export linkage from the module that IMPLEMENTs the hook, and
  * import linkage from external modules that link to the hook's module.
  */
-#define AP_IMPLEMENT_EXTERNAL_HOOK_RUN_FIRST(link,ret,name,args_decl,args_use,decline) \
-AP_IMPLEMENT_EXTERNAL_HOOK_BASE(link,name) \
-link##_DECLARE(ret) ap_run_##name args_decl \
+#define APR_IMPLEMENT_EXTERNAL_HOOK_RUN_FIRST(ns,link,ret,name,args_decl,args_use,decline) \
+APR_IMPLEMENT_EXTERNAL_HOOK_BASE(ns,link,name) \
+link##_DECLARE(ret) ns##_run_##name args_decl \
     { \
-    LINK_##name *pHook; \
+    ns##_LINK_##name##_t *pHook; \
     int n; \
     ret rv; \
 \
     if(!_hooks.link_##name) \
 	return decline; \
 \
-    pHook=(LINK_##name *)_hooks.link_##name->elts; \
+    pHook=(ns##_LINK_##name##_t *)_hooks.link_##name->elts; \
     for(n=0 ; n < _hooks.link_##name->nelts ; ++n) \
 	{ \
 	rv=pHook[n].pFunc args_use; \
@@ -207,44 +211,44 @@ link##_DECLARE(ret) ap_run_##name args_decl \
     }
 
      /* Hook orderings */
-#define AP_HOOK_REALLY_FIRST	(-10)
-#define AP_HOOK_FIRST		0
-#define AP_HOOK_MIDDLE		10
-#define AP_HOOK_LAST		20
-#define AP_HOOK_REALLY_LAST	30
+#define APR_HOOK_REALLY_FIRST	(-10)
+#define APR_HOOK_FIRST		0
+#define APR_HOOK_MIDDLE		10
+#define APR_HOOK_LAST		20
+#define APR_HOOK_REALLY_LAST	30
 
 /**
  * The global pool used to allocate any memory needed by the hooks.
- * @defvar apr_pool_t *ap_global_hook_pool
+ * @defvar apr_pool_t *apr_global_hook_pool
  */ 
-extern APU_DECLARE_DATA apr_pool_t *ap_global_hook_pool;
+extern APU_DECLARE_DATA apr_pool_t *apr_global_hook_pool;
 
 /**
  * A global variable to determine if debugging information about the
  * hooks functions should be printed
- * @defvar apr_pool_t *ap_debug_module_hooks
+ * @defvar apr_pool_t *apr_debug_module_hooks
  */ 
-extern APU_DECLARE_DATA int ap_debug_module_hooks;
+extern APU_DECLARE_DATA int apr_debug_module_hooks;
 
 /**
  * The name of the module that is currently registering a function
- * @defvar apr_pool_t *ap_debug_module_name
+ * @defvar apr_pool_t *apr_debug_module_name
  */ 
-extern APU_DECLARE_DATA const char *ap_current_hooking_module;
+extern APU_DECLARE_DATA const char *apr_debug_module_name;
 
 /**
  * Register a hook function to be sorted
  * @param szHookName The name of the Hook the function is registered for
  * @param aHooks The array which stores all of the functions for this hook
- * @deffunc void ap_hook_sort_register(const char *szHookName, ap_arry_header_t **aHooks)
+ * @deffunc void apr_hook_sort_register(const char *szHookName, apr_array_header_t **aHooks)
  */
-APU_DECLARE(void) ap_hook_sort_register(const char *szHookName, 
+APU_DECLARE(void) apr_hook_sort_register(const char *szHookName, 
                                         apr_array_header_t **aHooks);
 /**
  * Sort all of the registerd functions for a given hook
- * @deffunc void ap_sort_hooks(void)
+ * @deffunc void apr_sort_hooks(void)
  */
-APU_DECLARE(void) ap_sort_hooks(void);
+APU_DECLARE(void) apr_sort_hooks(void);
 
 /**
  * Print all of the information about the current hook.  This is used for
@@ -252,19 +256,19 @@ APU_DECLARE(void) ap_sort_hooks(void);
  * @param szName The name of the hook
  * @param aszPre All of the functions in the predecessor array
  * @param aszSucc All of the functions in the successor array
- * @deffunc void ap_show_hook(const char *szName, const char *const *aszPre, const char *const *aszSucc)
+ * @deffunc void apr_show_hook(const char *szName, const char *const *aszPre, const char *const *aszSucc)
  */
-APU_DECLARE(void) ap_show_hook(const char *szName,const char * const *aszPre,
+APU_DECLARE(void) apr_show_hook(const char *szName,const char * const *aszPre,
                                const char * const *aszSucc);
 
 /**
  * Remove all currently registered functions.
- * @deffunc void ap_hook_deregister_all(void)
+ * @deffunc void apr_hook_deregister_all(void)
  */
-APU_DECLARE(void) ap_hook_deregister_all(void);
+APU_DECLARE(void) apr_hook_deregister_all(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* ndef(AP_HOOKS_H) */
+#endif /* APR_HOOKS_H */
