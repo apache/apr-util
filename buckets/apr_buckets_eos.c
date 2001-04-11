7e559bdb82c243d30d9b9d701ed0f411508287e8
Cliff Woolley <jwoolley@apache.org>
2001-04-11 19:07:10 +0000

    Removed apr_bucket_do_create() macro, which was causing warnings about unreachable code in some compilers (notably MSVC).  What used to be done by this macro is now done inline in the various apr_bucket_foo_create() functions.  [Cliff Woolley]
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58209 13f79535-47bb-0310-9956-ffa450edef68

--- buckets/apr_buckets_eos.c
+++ buckets/apr_buckets_eos.c
@@ -81,7 +81,10 @@ APU_DECLARE(apr_bucket *) apr_bucket_eos_make(apr_bucket *b)
 
 APU_DECLARE(apr_bucket *) apr_bucket_eos_create(void)
 {
-    apr_bucket_do_create(apr_bucket_eos_make(b));
+    apr_bucket *b = (apr_bucket *)calloc(1, sizeof(*b));
+
+    APR_BUCKET_INIT(b);
+    return apr_bucket_eos_make(b);
 }
 
 APU_DECLARE_DATA const apr_bucket_type_t apr_bucket_type_eos = {
