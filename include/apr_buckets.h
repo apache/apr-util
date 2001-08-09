788c39950970baa42b9fbe360fa12b2c0072599b
Cliff Woolley <jwoolley@apache.org>
2001-08-09 23:55:27 +0000

    Misunderstandings about APR_BRIGADE_FOREACH() must be just about one of the most common problems people stumble on when they try to use the buckets API.  This adds a big honkin warning to the documentation for APR_BRIGADE_FOREACH and APR_RING_FOREACH that explains the limitations and gives an example for how to work around them.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58363 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_buckets.h
+++ include/apr_buckets.h
@@ -337,6 +337,22 @@ typedef apr_status_t (*apr_brigade_flush)(apr_bucket_brigade *bb, void *ctx);
  *	    ...
  * 	}
  * </pre>
+ * @warning Be aware that you *cannot* change the value of e within
+ * the foreach loop, nor can you destroy or otherwise modify the
+ * bucket pointed to by e.  If you do, then APR_BRIGADE_FOREACH
+ * will have no way to find out what bucket to use for its next
+ * iteration.  The reason for this can be seen by looking closely
+ * at the equivalent loops given in the tip above.  So, for example,
+ * if you are writing a loop that empties out a brigade one bucket
+ * at a time, APR_BRIGADE_FOREACH just won't work for you.  Do it
+ * by hand, like so:
+ * <pre>
+ *      while (!APR_BRIGADE_EMPTY(b)) {
+ *          e = APR_BRIGADE_FIRST(b);
+ *          ...
+ *          apr_bucket_delete(e);
+ *      }
+ * </pre>
  * @deffunc void APR_BRIGADE_FOREACH(apr_bucket *e, apr_bucket_brigade *b)
  */
 #define APR_BRIGADE_FOREACH(e, b)					\
