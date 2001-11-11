f04a2ca20ae9705214cd1e423591900ee32602cf
Greg Stein <gstein@apache.org>
2001-11-11 07:46:29 +0000

    * Copy a lot of code unchanged from apr_dbm.c to apr_dbm_*.c. Changes   occurred:   - open() and exists() split the #if logic out to the three files   - vt_db_fetch() had minor changes related to the 'rd' variable   - the various exists() functions were tweaked
    * the geterror() was eliminated as apr_dbm.c can handle it
    
    * Jeff Trawick's comments re: static vs APU_DECLARE_DATA for the
      vtables.
    
    * apu_conf.m4, dbm/Makefile.in: build/link in the appropriate dbm file
      (until we start buildig/linking all of them)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58448 13f79535-47bb-0310-9956-ffa450edef68

--- dbm/apr_dbm_sdbm.c
+++ dbm/apr_dbm_sdbm.c
@@ -52,12 +52,26 @@
  * <http://www.apache.org/>.
  */
 
+#include "apr_strings.h"
+#define APR_WANT_MEMFUNC
+#define APR_WANT_STRFUNC
+#include "apr_want.h"
+
 #include "apr_dbm_private.h"
 
 #include "apr_sdbm.h"
 #if APR_HAVE_STDLIB_H
 #include <stdlib.h>  /* For abort() */
 #endif
+
+
+/* this is used in a few places to define a noop "function". it is needed
+   to stop "no effect" warnings from GCC. */
+#define NOOP_FUNCTION if (0) ; else
+
+/* ### define defaults for now; these will go away in a while */
+#define REGISTER_CLEANUP(dbm, pdatum) NOOP_FUNCTION
+#define SET_FILE(pdb, f) ((pdb)->file = (f))
 
 typedef apr_sdbm_t *real_file_t;
 
@@ -102,11 +116,48 @@ static apr_status_t set_error(apr_dbm_t *dbm, apr_status_t dbm_said)
 ** DEFINE THE VTABLE FUNCTIONS FOR SDBM
 */
 
-static apr_status_t vt_sdbm_open(apr_dbm_t **dbm, const char *name,
+static apr_status_t vt_sdbm_open(apr_dbm_t **pdb, const char *pathname,
                                  apr_int32_t mode, apr_fileperms_t perm,
-                                 apr_pool_t *cntxt)
+                                 apr_pool_t *pool)
 {
-    abort();
+    real_file_t file;
+    int dbmode;
+
+    *pdb = NULL;
+
+    switch (mode) {
+    case APR_DBM_READONLY:
+        dbmode = APR_DBM_DBMODE_RO;
+        break;
+    case APR_DBM_READWRITE:
+        dbmode = APR_DBM_DBMODE_RW;
+        break;
+    case APR_DBM_RWCREATE:
+        dbmode = APR_DBM_DBMODE_RWCREATE;
+        break;
+    case APR_DBM_RWTRUNC:
+        dbmode = APR_DBM_DBMODE_RWTRUNC;
+        break;
+    default:
+        return APR_EINVAL;
+    }
+
+    {
+        apr_status_t rv;
+
+        rv = apr_sdbm_open(&file, pathname, dbmode, perm, pool);
+        if (rv != APR_SUCCESS)
+            return rv;
+    }
+
+    /* we have an open database... return it */
+    *pdb = apr_pcalloc(pool, sizeof(**pdb));
+    (*pdb)->pool = pool;
+    (*pdb)->type = &apr_dbm_type_sdbm;
+    SET_FILE(*pdb, file);
+
+    /* ### register a cleanup to close the DBM? */
+
     return APR_SUCCESS;
 }
 
@@ -118,46 +169,93 @@ static void vt_sdbm_close(apr_dbm_t *dbm)
 static apr_status_t vt_sdbm_fetch(apr_dbm_t *dbm, apr_datum_t key,
                                   apr_datum_t * pvalue)
 {
-    abort();
-    return APR_SUCCESS;
+    apr_status_t rv;
+    cvt_datum_t ckey;
+    result_datum_t rd;
+
+    CONVERT_DATUM(ckey, &key);
+    rv = APR_DBM_FETCH(dbm->file, ckey, rd);
+    RETURN_DATUM(pvalue, rd);
+
+    REGISTER_CLEANUP(dbm, pvalue);
+
+    /* store the error info into DBM, and return a status code. Also, note
+       that *pvalue should have been cleared on error. */
+    return set_error(dbm, rv);
 }
 
 static apr_status_t vt_sdbm_store(apr_dbm_t *dbm, apr_datum_t key,
                                   apr_datum_t value)
 {
-    abort();
-    return APR_SUCCESS;
+    apr_status_t rv;
+    cvt_datum_t ckey;
+    cvt_datum_t cvalue;
+
+    CONVERT_DATUM(ckey, &key);
+    CONVERT_DATUM(cvalue, &value);
+    rv = APR_DBM_STORE(dbm->file, ckey, cvalue);
+
+    /* store any error info into DBM, and return a status code. */
+    return set_error(dbm, rv);
 }
 
 static apr_status_t vt_sdbm_del(apr_dbm_t *dbm, apr_datum_t key)
 {
-    abort();
-    return APR_SUCCESS;
+    apr_status_t rv;
+    cvt_datum_t ckey;
+
+    CONVERT_DATUM(ckey, &key);
+    rv = APR_DBM_DELETE(dbm->file, ckey);
+
+    /* store any error info into DBM, and return a status code. */
+    return set_error(dbm, rv);
 }
 
 static int vt_sdbm_exists(apr_dbm_t *dbm, apr_datum_t key)
 {
-    abort();
-    return 0;
+    int exists;
+    apr_sdbm_datum_t *ckey = (apr_sdbm_datum_t *)&key;
+
+    {
+	apr_sdbm_datum_t value;
+        if (apr_sdbm_fetch(dbm->file, &value, *ckey) != APR_SUCCESS) {
+	    exists = 0;
+        }
+        else
+            exists = value.dptr != NULL;
+    }
+
+    return exists;
 }
 
 static apr_status_t vt_sdbm_firstkey(apr_dbm_t *dbm, apr_datum_t * pkey)
 {
-    abort();
-    return APR_SUCCESS;
+    apr_status_t rv;
+    result_datum_t rd;
+
+    rv = APR_DBM_FIRSTKEY(dbm->file, rd);
+    RETURN_DATUM(pkey, rd);
+
+    REGISTER_CLEANUP(dbm, pkey);
+
+    /* store any error info into DBM, and return a status code. */
+    return set_error(dbm, rv);
 }
 
 static apr_status_t vt_sdbm_nextkey(apr_dbm_t *dbm, apr_datum_t * pkey)
 {
-    abort();
-    return APR_SUCCESS;
-}
+    apr_status_t rv;
+    cvt_datum_t ckey;
+    result_datum_t rd;
 
-static char * vt_sdbm_geterror(apr_dbm_t *dbm, int *errcode, char *errbuf,
-                               apr_size_t errbufsize)
-{
-    abort();
-    return NULL;
+    CONVERT_DATUM(ckey, pkey);
+    rv = APR_DBM_NEXTKEY(dbm->file, ckey, rd);
+    RETURN_DATUM(pkey, rd);
+
+    REGISTER_CLEANUP(dbm, pkey);
+
+    /* store any error info into DBM, and return a status code. */
+    return set_error(dbm, APR_SUCCESS);
 }
 
 static void vt_sdbm_freedatum(apr_dbm_t *dbm, apr_datum_t data)
@@ -181,7 +279,7 @@ static void vt_sdbm_usednames(apr_pool_t *pool, const char *pathname,
 }
 
 
-static const apr_dbm_type_t apr_dbm_type_sdbm = {
+APU_DECLARE_DATA const apr_dbm_type_t apr_dbm_type_sdbm = {
     "sdbm",
 
     vt_sdbm_open,
@@ -192,7 +290,6 @@ static const apr_dbm_type_t apr_dbm_type_sdbm = {
     vt_sdbm_exists,
     vt_sdbm_firstkey,
     vt_sdbm_nextkey,
-    vt_sdbm_geterror,
     vt_sdbm_freedatum,
     vt_sdbm_usednames
 };
