62966bf969a2138c776dc77f0c26c6d3d83479bf
Brian Pane <brianp@apache.org>
2002-05-09 16:33:07 +0000

    Added apr_strmatch API
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58616 13f79535-47bb-0310-9956-ffa450edef68

---
+++ strmatch/apr_strmatch.c
@@ -0,0 +1,157 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "apr_strmatch.h"
+#include "apr_lib.h"
+#define APR_WANT_STRFUNC
+#include "apr_want.h"
+
+
+#define NUM_CHARS  256
+
+/* String searching functions
+ */
+APU_DECLARE_NONSTD(const char *) match_no_op(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen);
+APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen);
+APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool_nocase(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen);
+
+APU_DECLARE(const apr_strmatch_pattern *)
+     apr_strmatch_precompile(apr_pool_t *p, const char *s, int case_sensitive)
+{
+    apr_strmatch_pattern *pattern;
+    apr_size_t i;
+    int *shift;
+
+    pattern = apr_palloc(p, sizeof(*pattern));
+    pattern->pattern = s;
+    pattern->length = strlen(s);
+    if (pattern->length == 0) {
+        pattern->compare = match_no_op;
+        pattern->context = NULL;
+        return pattern;
+    }
+    else if (case_sensitive) {
+        pattern->compare = match_boyer_moore_horspool;
+    }
+    else {
+        pattern->compare = match_boyer_moore_horspool_nocase;
+    }
+
+    shift = (int *)apr_palloc(p, sizeof(int) * NUM_CHARS);
+    for (i = 0; i < NUM_CHARS; i++) {
+        shift[i] = pattern->length;
+    }
+    if (case_sensitive) {
+        for (i = 0; i < pattern->length - 1; i++) {
+            shift[(int)s[i]] = pattern->length - i - 1;
+        }
+    }
+    else {
+        for (i = 0; i < pattern->length - 1; i++) {
+            shift[apr_tolower(s[i])] = pattern->length - i - 1;
+        }
+    }
+    pattern->context = shift;
+
+    return pattern;
+}
+
+APU_DECLARE_NONSTD(const char *) match_no_op(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
+{
+    return s;
+}
+
+APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
+{
+    const char *s_end = s + slen;
+    int *shift = (int *)(this_pattern->context);
+    const char *s_next = s + this_pattern->length - 1;
+    const char *p_start = this_pattern->pattern;
+    const char *p_end = p_start + this_pattern->length - 1;
+    while (s_next < s_end) {
+        const char *s_tmp = s_next;
+        const char *p_tmp = p_end;
+        while (*s_tmp == *p_tmp) {
+            p_tmp--;
+            if (p_tmp < p_start) {
+                return s_tmp;
+            }
+            s_tmp--;
+        }
+        s_next += shift[(int)*s_next];
+    }
+    return NULL;
+}
+
+APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool_nocase(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
+{
+    const char *s_end = s + slen;
+    int *shift = (int *)(this_pattern->context);
+    const char *s_next = s + this_pattern->length - 1;
+    const char *p_start = this_pattern->pattern;
+    const char *p_end = p_start + this_pattern->length - 1;
+    while (s_next < s_end) {
+        const char *s_tmp = s_next;
+        const char *p_tmp = p_end;
+        while (apr_tolower(*s_tmp) == apr_tolower(*p_tmp)) {
+            p_tmp--;
+            if (p_tmp < p_start) {
+                return s_tmp;
+            }
+            s_tmp--;
+        }
+        s_next += shift[(int)*s_next];
+    }
+    return NULL;
+}
