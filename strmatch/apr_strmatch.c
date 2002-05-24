6e6151cf08465ebc19577e5da9cd356bd7091f25
Brian Pane <brianp@apache.org>
2002-05-24 01:07:47 +0000

    Switched to unsigned chars in the searching functions to avoid problems when input strings contain character values > 127 Reported by: Cliff Woolley
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@58649 13f79535-47bb-0310-9956-ffa450edef68

--- strmatch/apr_strmatch.c
+++ strmatch/apr_strmatch.c
@@ -110,14 +110,14 @@ APU_DECLARE_NONSTD(const char *) match_no_op(const apr_strmatch_pattern *this_pa
 
 APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
 {
-    const char *s_end = s + slen;
+    const unsigned char *s_end = s + slen;
     int *shift = (int *)(this_pattern->context);
-    const char *s_next = s + this_pattern->length - 1;
-    const char *p_start = this_pattern->pattern;
-    const char *p_end = p_start + this_pattern->length - 1;
+    const unsigned char *s_next = s + this_pattern->length - 1;
+    const unsigned char *p_start = this_pattern->pattern;
+    const unsigned char *p_end = p_start + this_pattern->length - 1;
     while (s_next < s_end) {
-        const char *s_tmp = s_next;
-        const char *p_tmp = p_end;
+        const unsigned char *s_tmp = s_next;
+        const unsigned char *p_tmp = p_end;
         while (*s_tmp == *p_tmp) {
             p_tmp--;
             if (p_tmp < p_start) {
@@ -132,14 +132,14 @@ APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool(const apr_strmatch_p
 
 APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool_nocase(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
 {
-    const char *s_end = s + slen;
+    const unsigned char *s_end = s + slen;
     int *shift = (int *)(this_pattern->context);
-    const char *s_next = s + this_pattern->length - 1;
-    const char *p_start = this_pattern->pattern;
-    const char *p_end = p_start + this_pattern->length - 1;
+    const unsigned char *s_next = s + this_pattern->length - 1;
+    const unsigned char *p_start = this_pattern->pattern;
+    const unsigned char *p_end = p_start + this_pattern->length - 1;
     while (s_next < s_end) {
-        const char *s_tmp = s_next;
-        const char *p_tmp = p_end;
+        const unsigned char *s_tmp = s_next;
+        const unsigned char *p_tmp = p_end;
         while (apr_tolower(*s_tmp) == apr_tolower(*p_tmp)) {
             p_tmp--;
             if (p_tmp < p_start) {
