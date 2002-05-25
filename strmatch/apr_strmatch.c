0a8ba956f6d1a430fc9d0ad7020d304be5e498d7
Cliff Woolley <jwoolley@apache.org>
2002-05-25 00:04:57 +0000

    These functions should be static.
    Reported by:  William Rowe
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@58653 13f79535-47bb-0310-9956-ffa450edef68

--- strmatch/apr_strmatch.c
+++ strmatch/apr_strmatch.c
@@ -60,14 +60,66 @@
 
 #define NUM_CHARS  256
 
-/* String searching functions
+/*
+ * String searching functions
  */
-APU_DECLARE_NONSTD(const char *) match_no_op(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen);
-APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen);
-APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool_nocase(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen);
+static const char *match_no_op(const apr_strmatch_pattern *this_pattern,
+                               const char *s, apr_size_t slen)
+{
+    return s;
+}
 
-APU_DECLARE(const apr_strmatch_pattern *)
-     apr_strmatch_precompile(apr_pool_t *p, const char *s, int case_sensitive)
+static const char *match_boyer_moore_horspool(
+                               const apr_strmatch_pattern *this_pattern,
+                               const char *s, apr_size_t slen)
+{
+    const char *s_end = s + slen;
+    int *shift = (int *)(this_pattern->context);
+    const char *s_next = s + this_pattern->length - 1;
+    const char *p_start = this_pattern->pattern;
+    const char *p_end = p_start + this_pattern->length - 1;
+    while (s_next < s_end) {
+        const char *s_tmp = s_next;
+        const char *p_tmp = p_end;
+        while (*s_tmp == *p_tmp) {
+            p_tmp--;
+            if (p_tmp < p_start) {
+                return s_tmp;
+            }
+            s_tmp--;
+        }
+        s_next += shift[(int)*((const unsigned char *)s_next)];
+    }
+    return NULL;
+}
+
+static const char *match_boyer_moore_horspool_nocase(
+                               const apr_strmatch_pattern *this_pattern,
+                               const char *s, apr_size_t slen)
+{
+    const char *s_end = s + slen;
+    int *shift = (int *)(this_pattern->context);
+    const char *s_next = s + this_pattern->length - 1;
+    const char *p_start = this_pattern->pattern;
+    const char *p_end = p_start + this_pattern->length - 1;
+    while (s_next < s_end) {
+        const char *s_tmp = s_next;
+        const char *p_tmp = p_end;
+        while (apr_tolower(*s_tmp) == apr_tolower(*p_tmp)) {
+            p_tmp--;
+            if (p_tmp < p_start) {
+                return s_tmp;
+            }
+            s_tmp--;
+        }
+        s_next += shift[(int)*((const unsigned char *)s_next)];
+    }
+    return NULL;
+}
+
+APU_DECLARE(const apr_strmatch_pattern *) apr_strmatch_precompile(
+                                              apr_pool_t *p, const char *s,
+                                              int case_sensitive)
 {
     apr_strmatch_pattern *pattern;
     apr_size_t i;
@@ -101,53 +153,4 @@ APU_DECLARE(const apr_strmatch_pattern *)
     pattern->context = shift;
 
     return pattern;
-}
-
-APU_DECLARE_NONSTD(const char *) match_no_op(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
-{
-    return s;
-}
-
-APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
-{
-    const char *s_end = s + slen;
-    int *shift = (int *)(this_pattern->context);
-    const char *s_next = s + this_pattern->length - 1;
-    const char *p_start = this_pattern->pattern;
-    const char *p_end = p_start + this_pattern->length - 1;
-    while (s_next < s_end) {
-        const char *s_tmp = s_next;
-        const char *p_tmp = p_end;
-        while (*s_tmp == *p_tmp) {
-            p_tmp--;
-            if (p_tmp < p_start) {
-                return s_tmp;
-            }
-            s_tmp--;
-        }
-        s_next += shift[(int)*((const unsigned char *)s_next)];
-    }
-    return NULL;
-}
-
-APU_DECLARE_NONSTD(const char *) match_boyer_moore_horspool_nocase(const apr_strmatch_pattern *this_pattern, const char *s, apr_size_t slen)
-{
-    const char *s_end = s + slen;
-    int *shift = (int *)(this_pattern->context);
-    const char *s_next = s + this_pattern->length - 1;
-    const char *p_start = this_pattern->pattern;
-    const char *p_end = p_start + this_pattern->length - 1;
-    while (s_next < s_end) {
-        const char *s_tmp = s_next;
-        const char *p_tmp = p_end;
-        while (apr_tolower(*s_tmp) == apr_tolower(*p_tmp)) {
-            p_tmp--;
-            if (p_tmp < p_start) {
-                return s_tmp;
-            }
-            s_tmp--;
-        }
-        s_next += shift[(int)*((const unsigned char *)s_next)];
-    }
-    return NULL;
 }
