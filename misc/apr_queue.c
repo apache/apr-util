135ae9e754b6bac62b3f0cfbd57a6ff4d02b3ca1
Ian Holsman <ianh@apache.org>
2002-09-06 23:51:56 +0000

    clean up a comment
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58744 13f79535-47bb-0310-9956-ffa450edef68

--- misc/apr_queue.c
+++ misc/apr_queue.c
@@ -127,9 +127,8 @@ static apr_status_t queue_destroy(void *data)
 {
     apr_queue_t *queue = data;
 
-    /* Ignore errors here, we can't do anything about them anyway.
-     * XXX: We should at least try to signal an error here, it is
-     * indicative of a programmer error. -aaron */
+    /* Ignore errors here, we can't do anything about them anyway. */
+
     apr_thread_cond_destroy(queue->not_empty);
     apr_thread_cond_destroy(queue->not_full);
     apr_thread_mutex_destroy(queue->one_big_mutex);
