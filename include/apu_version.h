a82f470c98e2a18c1ee5a456a3d35aada6940624
Justin Erenkrantz <jerenkrantz@apache.org>
2002-09-12 00:46:00 +0000

    Move to 0.9.2-dev
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@58749 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -101,14 +101,14 @@ extern "C" {
 #define APU_MINOR_VERSION       9
 
 /** patch level */
-#define APU_PATCH_VERSION       1
+#define APU_PATCH_VERSION       2
 
 
 /** 
  *  This symbol is defined for internal, "development" copies of APU. This
  *  symbol will be #undef'd for releases. 
  */
-#undef APU_IS_DEV_VERSION
+#define APU_IS_DEV_VERSION
 
 /** The formatted string of APU's version */
 #define APU_VERSION_STRING \
