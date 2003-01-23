ead84c3f3cf2e83e97bcb43dd4bcf96facaf38f4
Branko Cibej <brane@apache.org>
2003-01-23 21:00:38 +0000

    Make testxlate.c mode modular and add latin1<->latin2 identity test.
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@58838 13f79535-47bb-0310-9956-ffa450edef68

--- test/testxlate.c
+++ test/testxlate.c
@@ -56,30 +56,27 @@
 #include <stdlib.h>
 
 #include "apr.h"
-#include "apr_general.h"
 #include "apr_errno.h"
+#include "apr_general.h"
+#include "apr_strings.h"
 #include "apr_xlate.h"
-
 
 static const char test_utf8[] = "Edelwei\xc3\x9f";
 static const char test_utf7[] = "Edelwei+AN8-";
 static const char test_latin1[] = "Edelwei\xdf";
+static const char test_latin2[] = "Edelwei\xdf";
 
 
 static int check_status (apr_status_t status, const char *msg)
 {
-    if (!status)
-    {
-        printf("PASS: %s\n", msg);
-        return 0;
-    }
-    else
+    if (status)
     {
         static char buf[1024];
-        printf("FAIL: %s\n      %s\n", msg,
+        printf("ERROR: %s\n      %s\n", msg,
                apr_strerror(status, buf, sizeof(buf)));
         return 1;
     }
+    return 0;
 }
 
 static int test_conversion (apr_xlate_t *convset,
@@ -99,23 +96,34 @@ static int test_conversion (apr_xlate_t *convset,
     if ((!status || APR_STATUS_IS_INCOMPLETE(status))
         && strcmp(buf, expected))
     {
-        printf("FAIL: expected: '%s'\n      actual:   '%s'"
-               "\n      inbytes_left: %"APR_SIZE_T_FMT"\n",
+        printf("ERROR: expected: '%s'\n       actual:   '%s'"
+               "\n       inbytes_left: %"APR_SIZE_T_FMT"\n",
                expected, buf, inbytes_left);
         retcode |= 1;
-    }
-    else
-    {
-        printf("PASS: Expected and actual output match\n");
     }
     return retcode;
 }
 
+static int one_test (const char *cs1, const char *cs2,
+                     const char *str1, const char *str2,
+                     apr_pool_t *pool)
+{
+    apr_xlate_t *convset;
+    const char *msg = apr_psprintf(pool, "apr_xlate_open(%s, %s)", cs2, cs1);
+    int retcode = check_status(apr_xlate_open(&convset, cs2, cs1, pool), msg);
+    if (!retcode)
+    {
+        retcode |= test_conversion(convset, str1, str2);
+        retcode |= check_status(apr_xlate_close(convset), "apr_xlate_close");
+    }
+    printf("%s:  %s -> %s\n", (retcode ? "FAIL" : "PASS"), cs1, cs2);
+    return retcode;
+}
+
+
 int main (int argc, char **argv)
 {
     apr_pool_t *pool;
-    apr_xlate_t *convset;
-    apr_status_t status;
     int retcode = 0;
 
 #ifndef APR_HAS_XLATE
@@ -128,47 +136,21 @@ int main (int argc, char **argv)
     apr_pool_create(&pool, NULL);
 
     /* 1. Identity transformation: UTF-8 -> UTF-8 */
-    puts("START: identity transformation");
-    status = apr_xlate_open(&convset, "UTF-8", "UTF-8", pool);
-    retcode |= check_status(status, "apr_xlate_open(UTF-8, UTF-8)");
-    if (!status)
-    {
-        retcode |= test_conversion(convset, test_utf8, test_utf8);
-        retcode |= check_status(apr_xlate_close(convset), "apr_xlate_close");
-    }
-    puts("END: identity transformation");
+    retcode |= one_test("UTF-8", "UTF-8", test_utf8, test_utf8, pool);
 
-    /* 2. UTF-8 -> ISO-8859-1 */
-    puts("START: UTF-8 -> ISO-8859-1");
-    status = apr_xlate_open(&convset, "ISO-8859-1", "UTF-8", pool);
-    retcode |= check_status(status, "apr_xlate_open(ISO-8859-1, UTF-8)");
-    if (!status)
-    {
-        retcode |= test_conversion(convset, test_utf8, test_latin1);
-        retcode |= check_status(apr_xlate_close(convset), "apr_xlate_close");
-    }
-    puts("END: UTF-8 -> ISO-8859-1");
+    /* 2. UTF-8 <-> ISO-8859-1 */
+    retcode |= one_test("UTF-8", "ISO-8859-1", test_utf8, test_latin1, pool);
+    retcode |= one_test("ISO-8859-1", "UTF-8", test_latin1, test_utf8, pool);
 
-    /* 3. Transformation using charset aliases */
-    puts("START: UTF-8 -> UTF-7 (alias)");
-    status = apr_xlate_open(&convset, "UTF-7", "UTF-8", pool);
-    retcode |= check_status(status, "apr_xlate_open(UTF-7, UTF-8)");
-    if (!status)
-    {
-        retcode |= test_conversion(convset, test_utf8, test_utf7);
-        retcode |= check_status(apr_xlate_close(convset), "apr_xlate_close");
-    }
-    puts("END: UTF-8 -> UTF-7 (alias)");
+    /* 3. ISO-8859-1 <-> ISO-8859-2, identity */
+    retcode |= one_test("ISO-8859-1", "ISO-8859-2",
+                        test_latin1, test_latin2, pool);
+    retcode |= one_test("ISO-8859-2", "ISO-8859-1",
+                        test_latin2, test_latin1, pool);
 
-    puts("START: UTF-7 (alias) -> UTF-8");
-    status = apr_xlate_open(&convset, "UTF-8", "UTF-7", pool);
-    retcode |= check_status(status, "apr_xlate_open(UTF-8, UTF-7)");
-    if (!status)
-    {
-        retcode |= test_conversion(convset, test_utf7, test_utf8);
-        retcode |= check_status(apr_xlate_close(convset), "apr_xlate_close");
-    }
-    puts("END: UTF-7 (alias) -> UTF-8");
+    /* 4. Transformation using charset aliases */
+    retcode |= one_test("UTF-8", "UTF-7", test_utf8, test_utf7, pool);
+    retcode |= one_test("UTF-7", "UTF-8", test_utf7, test_utf8, pool);
 
     return retcode;
 }
