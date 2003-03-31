4b59243b70e86bf8a72ef291be961e989896ff92
William A. Rowe Jr <wrowe@apache.org>
2003-03-31 05:33:34 +0000

    On to 0.9.4-dev
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@58881 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -101,13 +101,13 @@ extern "C" {
 #define APU_MINOR_VERSION       9
 
 /** patch level */
-#define APU_PATCH_VERSION       3
+#define APU_PATCH_VERSION       4
 
 /** 
  *  This symbol is defined for internal, "development" copies of APU. This
  *  symbol will be #undef'd for releases. 
  */
-/* #undef APU_IS_DEV_VERSION */
+#define APU_IS_DEV_VERSION
 
 
 
