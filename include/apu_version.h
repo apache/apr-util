11b5fada1c14dba71b17b1c7808b1da5148fbde0
Justin Erenkrantz <jerenkrantz@apache.org>
2003-09-26 07:43:54 +0000

    HEAD is 1.0 not 0.9.4.
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@58939 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -92,16 +92,16 @@ extern "C" {
  * programs such as structure size changes.  No binary compatibility is
  * possible across a change in the major version.
  */
-#define APU_MAJOR_VERSION       0
+#define APU_MAJOR_VERSION       1
 
 /** 
  * Minor API changes that do not cause binary compatibility problems.
  * Should be reset to 0 when upgrading APU_MAJOR_VERSION
  */
-#define APU_MINOR_VERSION       9
+#define APU_MINOR_VERSION       0
 
 /** patch level */
-#define APU_PATCH_VERSION       4
+#define APU_PATCH_VERSION       0
 
 /** 
  *  This symbol is defined for internal, "development" copies of APU. This
