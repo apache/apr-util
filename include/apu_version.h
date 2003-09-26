13a3d544f97c4dcad553e4d722c7c0d4829893b7
(no author) <(no author)@unknown>
2003-09-26 05:43:56 +0000

    This commit was manufactured by cvs2svn to create tag 'APACHE_2_0_48'.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/tags/APACHE_2_0_48@58938 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -101,7 +101,7 @@ extern "C" {
 #define APU_MINOR_VERSION       9
 
 /** patch level */
-#define APU_PATCH_VERSION       4
+#define APU_PATCH_VERSION       5
 
 /** 
  *  This symbol is defined for internal, "development" copies of APU. This
