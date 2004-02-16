532409e6a83243fcfb2594282d94941499c7b257
Joe Orton <jorton@apache.org>
2004-02-16 15:54:50 +0000

    * dbm/apr_dbm_sdbm.c (vt_sdbm_exists): Fix (size_t *) -> (int *) casts missed in earlier fix for PR 14861. (broken only on bigendian LP64 platforms)
    * test/testdb.c (doit): Add regression test.
    
    * test/Makefile.in: Link test programs using -no-install.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@58990 13f79535-47bb-0310-9956-ffa450edef68

--- dbm/apr_dbm_sdbm.c
+++ dbm/apr_dbm_sdbm.c
@@ -179,11 +179,13 @@ static apr_status_t vt_sdbm_del(apr_dbm_t *dbm, apr_datum_t key)
 static int vt_sdbm_exists(apr_dbm_t *dbm, apr_datum_t key)
 {
     int exists;
-    apr_sdbm_datum_t *ckey = (apr_sdbm_datum_t *)&key;
+    apr_sdbm_datum_t ckey;
+
+    CONVERT_DATUM(ckey, &key);
 
     {
         apr_sdbm_datum_t value;
-        if (apr_sdbm_fetch(dbm->file, &value, *ckey) != APR_SUCCESS) {
+        if (apr_sdbm_fetch(dbm->file, &value, ckey) != APR_SUCCESS) {
             exists = 0;
         }
         else
