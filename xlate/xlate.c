ccdc0135cc64b69f35b4530faeb398636d19b18c
Joe Orton <jorton@apache.org>
2004-06-06 10:07:07 +0000

    * xlate/xlate.c (check_sbcs): Reinstate function with fix to reset iconv handle per 0.9 branch pending any progress on API issues.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@59048 13f79535-47bb-0310-9956-ffa450edef68

--- xlate/xlate.c
+++ xlate/xlate.c
@@ -101,6 +101,89 @@ static apr_status_t apr_xlate_cleanup(void *convset)
     return APR_SUCCESS;
 }
 
+#if APU_HAVE_ICONV
+static void check_sbcs(apr_xlate_t *convset)
+{
+    char inbuf[256], outbuf[256];
+    char *inbufptr = inbuf;
+    char *outbufptr = outbuf;
+    apr_size_t inbytes_left, outbytes_left;
+    int i;
+    apr_size_t translated;
+
+    for (i = 0; i < sizeof(inbuf); i++) {
+        inbuf[i] = i;
+    }
+
+    inbytes_left = outbytes_left = sizeof(inbuf);
+    translated = iconv(convset->ich, (ICONV_INBUF_TYPE)&inbufptr,
+                       &inbytes_left, &outbufptr, &outbytes_left);
+
+    if (translated != (apr_size_t)-1
+        && inbytes_left == 0
+        && outbytes_left == 0) {
+        /* hurray... this is simple translation; save the table,
+         * close the iconv descriptor
+         */
+
+        convset->sbcs_table = apr_palloc(convset->pool, sizeof(outbuf));
+        memcpy(convset->sbcs_table, outbuf, sizeof(outbuf));
+        iconv_close(convset->ich);
+        convset->ich = (iconv_t)-1;
+
+        /* TODO: add the table to the cache */
+    }
+    else {
+        /* reset the iconv descriptor, since it's now in an undefined
+         * state. */
+        iconv_close(convset->ich);
+        convset->ich = iconv_open(convset->topage, convset->frompage);
+    }
+}
+#elif APU_HAVE_APR_ICONV
+static void check_sbcs(apr_xlate_t *convset)
+{
+    char inbuf[256], outbuf[256];
+    char *inbufptr = inbuf;
+    char *outbufptr = outbuf;
+    apr_size_t inbytes_left, outbytes_left;
+    int i;
+    apr_size_t translated;
+    apr_status_t rv;
+
+    for (i = 0; i < sizeof(inbuf); i++) {
+        inbuf[i] = i;
+    }
+
+    inbytes_left = outbytes_left = sizeof(inbuf);
+    rv = apr_iconv(convset->ich, (ICONV_INBUF_TYPE)&inbufptr,
+                   &inbytes_left, &outbufptr, &outbytes_left,
+                   &translated);
+
+    if ((rv == APR_SUCCESS)
+        && (translated != (apr_size_t)-1)
+        && inbytes_left == 0
+        && outbytes_left == 0) {
+        /* hurray... this is simple translation; save the table,
+         * close the iconv descriptor
+         */
+
+        convset->sbcs_table = apr_palloc(convset->pool, sizeof(outbuf));
+        memcpy(convset->sbcs_table, outbuf, sizeof(outbuf));
+        apr_iconv_close(convset->ich, convset->pool);
+        convset->ich = (apr_iconv_t)-1;
+
+        /* TODO: add the table to the cache */
+    }
+    else {
+        /* reset the iconv descriptor, since it's now in an undefined
+         * state. */
+        apr_iconv_close(convset->ich);
+        convset->ich = apr_iconv_open(convset->topage, convset->frompage);
+    }
+}
+#endif /* APU_HAVE_APR_ICONV */
+
 static void make_identity_table(apr_xlate_t *convset)
 {
   int i;
@@ -157,6 +240,7 @@ APU_DECLARE(apr_status_t) apr_xlate_open(apr_xlate_t **convset,
             return rv;
         }
         found = 1;
+        check_sbcs(new);
     } else
         new->ich = (apr_iconv_t)-1;
 
@@ -169,6 +253,7 @@ APU_DECLARE(apr_status_t) apr_xlate_open(apr_xlate_t **convset,
             return rv ? rv : APR_EINVAL;
         }
         found = 1;
+        check_sbcs(new);
     } else
         new->ich = (iconv_t)-1;
 #endif /* APU_HAVE_ICONV */
