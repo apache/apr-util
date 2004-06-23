646bb216dd8204a6afebbc34b754e9553dc8b3e8
Joe Orton <jorton@apache.org>
2004-06-23 09:53:43 +0000

    * test/testpass.c: Adapted to abts framework; add tests for MD5 and SHA1 passwords.
    * test/abts_tests.h, test/Makefile.in, test/testutil.h: Integrate
    testpass.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@59072 13f79535-47bb-0310-9956-ffa450edef68

--- test/testutil.h
+++ test/testutil.h
@@ -46,5 +46,6 @@ abts_suite *teststrmatch(abts_suite *suite);
 abts_suite *testuri(abts_suite *suite);
 abts_suite *testuuid(abts_suite *suite);
 abts_suite *testbuckets(abts_suite *suite);
+abts_suite *testpass(abts_suite *suite);
 
 #endif /* APR_TEST_INCLUDES */
