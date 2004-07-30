c63aead5b50a6d30ae55f29b6a0bcd1a9bfddf9a
Graham Leggett <minfrin@apache.org>
2004-07-30 19:40:44 +0000

    Add APR functions to do the job of ldap_init(), hiding toolkit specific SSL/TLS handling. Code derived from httpd util_ldap. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@59099 13f79535-47bb-0310-9956-ffa450edef68

---
+++ include/apr_ldap_init.h
@@ -0,0 +1,34 @@
+/* Copyright 2000-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef APR_LDAP_INIT_H
+#define APR_LDAP_INIT_H
+
+#include "apr_ldap.h"
+
+#if APR_HAS_LDAP
+
+/*
+ * The following defines handle the different certificate file
+ * types that might be used when SSL support is included.
+ */
+#define APR_LDAP_CA_TYPE_UNKNOWN  0
+#define APR_LDAP_CA_TYPE_DER      1
+#define APR_LDAP_CA_TYPE_BASE64   2
+#define APR_LDAP_CA_TYPE_CERT7_DB 3
+
+#endif /* APR_HAS_LDAP */
+
+#endif /* APR_LDAP_URL_H */
