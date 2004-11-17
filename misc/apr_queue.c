2e7bd51bbd57d6cc59dc4c226523ead3e6f11232
Joe Orton <jorton@apache.org>
2004-11-17 12:33:02 +0000

    Avoid testing undefined symbols.  No functional change.
    * apr-util/misc/apr_queue.c
      Include the header that defines the symbols we are testing.
    
    Submitted by: Julian Foad <julianfoad btopenworld.com>
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@76122 13f79535-47bb-0310-9956-ffa450edef68

--- misc/apr_queue.c
+++ misc/apr_queue.c
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include "apr.h"
+
 #if APR_HAVE_STDIO_H
 #include <stdio.h>
 #endif
