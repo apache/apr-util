fab4bf72171f8ae6ac395f8874b7565f3639e1a4
Bradley Nicholes <bnicholes@apache.org>
2005-01-11 19:43:12 +0000

    Add the NetWare build files and a test suite for the LDAP APIs
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@124917 13f79535-47bb-0310-9956-ffa450edef68

--- test/testutil.h
+++ test/testutil.h
@@ -49,5 +49,6 @@ abts_suite *testbuckets(abts_suite *suite);
 abts_suite *testpass(abts_suite *suite);
 abts_suite *testmd4(abts_suite *suite);
 abts_suite *testmd5(abts_suite *suite);
+abts_suite *testldap(abts_suite *suite);
 
 #endif /* APR_TEST_INCLUDES */
