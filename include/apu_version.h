cb961cc4c8403e8070e7f1e4153698262e67c38e
Justin Erenkrantz <jerenkrantz@apache.org>
2005-02-04 21:55:23 +0000

    APR-util's is off to 0.9.7-dev land.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/0.9.x@151428 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -63,13 +63,13 @@ extern "C" {
 #define APU_MINOR_VERSION       9
 
 /** patch level */
-#define APU_PATCH_VERSION       6
+#define APU_PATCH_VERSION       7
 
 /** 
  *  This symbol is defined for internal, "development" copies of APU. This
  *  symbol will be #undef'd for releases. 
  */
-/* #define APU_IS_DEV_VERSION */
+#define APU_IS_DEV_VERSION
 
 
 /** The formatted string of APU's version */
