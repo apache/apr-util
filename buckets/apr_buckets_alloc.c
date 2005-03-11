df1e08f3ea1a1e172be7f665d03ac77051bca9b7
Cliff Woolley <jwoolley@apache.org>
2005-03-11 20:38:19 +0000

    Fix memory leak when in APR_POOL_DEBUG mode.
    Submitted by: Joe Schaefer
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@157149 13f79535-47bb-0310-9956-ffa450edef68

--- buckets/apr_buckets_alloc.c
+++ buckets/apr_buckets_alloc.c
@@ -46,6 +46,13 @@ static apr_status_t alloc_cleanup(void *data)
     apr_bucket_alloc_t *list = data;
 
     apr_allocator_free(list->allocator, list->blocks);
+
+#if APR_POOL_DEBUG
+    if (list->pool && list->allocator != apr_pool_allocator_get(list->pool)) {
+        apr_allocator_destroy(list->allocator);
+    }
+#endif
+
     return APR_SUCCESS;
 }
 
