52eaa2b659609d4cea2de70ddf77bf72d03a3bb4
Nick Kew <niq@apache.org>
2005-09-02 00:22:56 +0000

    Fix EscapeString (Patch from Brian J. France)
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@265809 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_pgsql.c
+++ dbd/apr_dbd_pgsql.c
@@ -221,7 +221,7 @@ static const char *dbd_pgsql_escape(apr_pool_t *pool, const char *arg,
                                     apr_dbd_t *sql)
 {
     size_t len = strlen(arg);
-    char *ret = apr_palloc(pool, len + 1);
+    char *ret = apr_palloc(pool, 2*len + 2);
     PQescapeString(ret, arg, len);
     return ret;
 }
