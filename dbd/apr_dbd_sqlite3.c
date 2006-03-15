2b2ab5be149753118a157a34a738be56e7e87e73
Garrett Rooney <rooneg@apache.org>
2006-03-15 06:04:54 +0000

    Merge r384327 from trunk into the 1.2.x branch.
    Original log message:
    
    Add or correct bounds checking for sqlite dbd backends.
    
    Submitted by: Bojan Smojver <bojan rexursive.com>
    
    * dbd/apr_dbd_sqlite2.c
      (dbd_sqlite_get_entry): Add bounds checking.
    
    * dbd/apr_dbd_sqlite3.c
      (dbd_sqlite3_get_entry): Correct bounds checking on negative values.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.2.x@385977 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_sqlite3.c
+++ dbd/apr_dbd_sqlite3.c
@@ -218,7 +218,7 @@ static const char *dbd_sqlite3_get_entry(const apr_dbd_row_t *row, int n)
 {
     apr_dbd_column_t *column;
     const char *value;
-    if ((n + 1) > row->columnCount) {
+    if ((n < 0) || (n >= row->columnCount)) {
         return NULL;
     }
     column = row->columns[n];
