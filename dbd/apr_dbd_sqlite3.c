925777881f8ffd1c7a40421943481c67b5637913
Bojan Smojver <bojan@apache.org>
2006-06-14 06:53:08 +0000

    Delay getting parameter count until after transaction check.
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@414125 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_sqlite3.c
+++ dbd/apr_dbd_sqlite3.c
@@ -403,12 +403,13 @@ static int dbd_sqlite3_pvquery(apr_pool_t *pool, apr_dbd_t *sql, int *nrows,
                                apr_dbd_prepared_t *statement, va_list args)
 {
     const char **values;
-    int i, nargs = sqlite3_bind_parameter_count(statement->stmt);
+    int i, nargs;
 
     if (sql->trans && sql->trans->errnum) {
         return sql->trans->errnum;
     }
 
+    nargs = sqlite3_bind_parameter_count(statement->stmt);
     values = apr_palloc(pool, sizeof(*values) * nargs);
 
     for (i = 0; i < nargs; i++) {
@@ -538,12 +539,13 @@ static int dbd_sqlite3_pvselect(apr_pool_t *pool, apr_dbd_t *sql,
                                 va_list args)
 {
     const char **values;
-    int i, nargs = sqlite3_bind_parameter_count(statement->stmt);
+    int i, nargs;
 
     if (sql->trans && sql->trans->errnum) {
         return sql->trans->errnum;
     }
 
+    nargs = sqlite3_bind_parameter_count(statement->stmt);
     values = apr_palloc(pool, sizeof(*values) * nargs);
 
     for (i = 0; i < nargs; i++) {
