2af677f1ef006f586e5de833f6d178f703aa8f32
Bojan Smojver <bojan@apache.org>
2006-06-14 06:55:06 +0000

    Merge r413798 and r414125 from trunk to 1.2.x branch. Original message: Don't expect variable argument list to end with NULL for PostgreSQL and SQLite3 DBD drivers. Count instead.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.2.x@414126 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_pgsql.c
+++ dbd/apr_dbd_pgsql.c
@@ -63,6 +63,7 @@ struct apr_dbd_row_t {
 struct apr_dbd_prepared_t {
     const char *name;
     int prepared;
+    int nargs;
 };
 
 #define dbd_pgsql_is_success(x) (((x) == PGRES_EMPTY_QUERY) \
@@ -247,7 +248,6 @@ static int dbd_pgsql_prepare(apr_pool_t *pool, apr_dbd_t *sql,
     size_t i = 0;
     const char *args[QUERY_MAX_ARGS];
     size_t alen;
-    int nargs = 0;
     int ret;
     PGresult *res;
     char *pgquery;
@@ -256,11 +256,12 @@ static int dbd_pgsql_prepare(apr_pool_t *pool, apr_dbd_t *sql,
     if (!*statement) {
         *statement = apr_palloc(pool, sizeof(apr_dbd_prepared_t));
     }
+    (*statement)->nargs = 0;
     /* Translate from apr_dbd to native query format */
     for (sqlptr = (char*)query; *sqlptr; ++sqlptr) {
         if (sqlptr[0] == '%') {
             if (isalpha(sqlptr[1])) {
-                ++nargs;
+                ++(*statement)->nargs;
             }
             else if (sqlptr[1] == '%') {
                 ++sqlptr;
@@ -268,8 +269,8 @@ static int dbd_pgsql_prepare(apr_pool_t *pool, apr_dbd_t *sql,
         }
     }
     length = strlen(query) + 1;
-    if (nargs > 8) {
-        length += nargs - 8;
+    if ((*statement)->nargs > 8) {
+        length += (*statement)->nargs - 8;
     }
     pgptr = pgquery = apr_palloc(pool, length) ;
 
@@ -324,10 +325,10 @@ static int dbd_pgsql_prepare(apr_pool_t *pool, apr_dbd_t *sql,
     length = strlen(label);
     memcpy(sqlptr, label, length);
     sqlptr += length;
-    if (nargs > 0) {
+    if ((*statement)->nargs > 0) {
         memcpy(sqlptr, " (",2);
         sqlptr += 2;
-        for (i=0; i<nargs; ++i) {
+        for (i=0; i < (*statement)->nargs; ++i) {
             alen = strlen(args[i]);
             memcpy(sqlptr, args[i], alen);
             sqlptr += alen;
@@ -399,22 +400,21 @@ static int dbd_pgsql_pvquery(apr_pool_t *pool, apr_dbd_t *sql,
                              int *nrows, apr_dbd_prepared_t *statement,
                              va_list args)
 {
-    const char *arg;
-    int nargs = 0;
-    const char *values[QUERY_MAX_ARGS];
+    const char **values;
+    int i;
 
     if (sql->trans && sql->trans->errnum) {
         return sql->trans->errnum;
     }
-    while ( arg = va_arg(args, const char*), arg ) {
-        if ( nargs >= QUERY_MAX_ARGS) {
-            va_end(args);
-            return -1;
-        }
-        values[nargs++] = apr_pstrdup(pool, arg);
+
+    values = apr_palloc(pool, sizeof(*values) * statement->nargs);
+
+    for (i = 0; i < statement->nargs; i++) {
+        values[i] = apr_pstrdup(pool, va_arg(args, const char*));
     }
-    values[nargs] = NULL;
-    return dbd_pgsql_pquery(pool, sql, nrows, statement, nargs, values);
+
+    return dbd_pgsql_pquery(pool, sql, nrows, statement,
+                            statement->nargs, values);
 }
 
 static int dbd_pgsql_pselect(apr_pool_t *pool, apr_dbd_t *sql,
@@ -500,23 +500,21 @@ static int dbd_pgsql_pvselect(apr_pool_t *pool, apr_dbd_t *sql,
                               apr_dbd_prepared_t *statement,
                               int seek, va_list args)
 {
-    const char *arg;
-    int nargs = 0;
-    const char *values[QUERY_MAX_ARGS];
+    const char **values;
+    int i;
 
     if (sql->trans && sql->trans->errnum) {
         return sql->trans->errnum;
     }
 
-    while (arg = va_arg(args, const char*), arg) {
-        if ( nargs >= QUERY_MAX_ARGS) {
-            va_end(args);
-            return -1;
-        }
-        values[nargs++] = apr_pstrdup(pool, arg);
+    values = apr_palloc(pool, sizeof(*values) * statement->nargs);
+
+    for (i = 0; i < statement->nargs; i++) {
+        values[i] = apr_pstrdup(pool, va_arg(args, const char*));
     }
+
     return dbd_pgsql_pselect(pool, sql, results, statement,
-                             seek, nargs, values) ;
+                             seek, statement->nargs, values) ;
 }
 
 static int dbd_pgsql_start_transaction(apr_pool_t *pool, apr_dbd_t *handle,
