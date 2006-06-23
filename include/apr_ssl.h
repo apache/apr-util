3d0f9211fc792662e0b45698efc37eda990db4d8
David Reid <dreid@apache.org>
2006-06-23 10:43:03 +0000

    Add basis of pollset support.
    This basically just allows us to pass ssl sockets to a pollset, which is
    possible as they are essentially just apr_socket with an additional layer.
    Hopefully this apporach will work for all the libs we need to support.
    
    Tidy up test app and add very basic pollset support. This test needs to be rewritten
    to actually do something useful.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@416667 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_ssl.h
+++ include/apr_ssl.h
@@ -22,6 +22,7 @@
 #include "apr_errno.h"
 #include "apr_pools.h"
 #include "apr_network_io.h"
+#include "apr_poll.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -211,6 +212,28 @@ APU_DECLARE(apr_status_t) apr_ssl_socket_accept(apr_ssl_socket_t **,
  */
 APU_DECLARE(apr_status_t) apr_ssl_socket_raw_error(apr_ssl_socket_t *);
 
+
+/**
+ * @fn apr_status_t apr_pollset_add_ssl_socket(apr_pollset_t *pollset
+                                               apr_ssl_socket_t *sock)
+ * @brief Add an ssl socket to a pollset.
+ * @param pollset The pollset to add the socket to.
+ * @param sock The ssl socket to add.
+ * @note This fucntion adds the socket with APR_POLLIN and APR_POLLOUT
+ *       set.
+ */
+APU_DECLARE(apr_status_t) apr_pollset_add_ssl_socket(apr_pollset_t *,
+                                                     apr_ssl_socket_t *);
+
+/**
+ * @fn apr_status_t apr_pollset_remove_ssl_socket(apr_pollset_t *pollset,
+                                                  apr_ssl_socket_t *sock)
+ * @brief remove an ssl socket from a pollset.
+ * @param pollset The pollset to remove the socket from.
+ * @param sock The ssl socket to remove.
+ */
+APU_DECLARE(apr_status_t) apr_pollset_remove_ssl_socket(apr_pollset_t *,
+                                                        apr_ssl_socket_t *);
 
 /** @} */
 #ifdef __cplusplus
