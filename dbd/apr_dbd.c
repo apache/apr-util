33d5370a288b5c122ba53dd846e3c44eb6a57c27
Guenter Knauf <fuankg@apache.org>
2007-07-10 23:56:43 +0000

    reverted r553027; changed apr_snprintf() for driver load as suggested by Martin.
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@555117 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd.c
+++ dbd/apr_dbd.c
@@ -150,13 +150,19 @@ APU_DECLARE(apr_status_t) apr_dbd_get_driver(apr_pool_t *pool, const char *name,
     /* The driver DSO must have exactly the same lifetime as the
      * drivers hash table; ignore the passed-in pool */
     pool = apr_hash_pool_get(drivers);
-    /* APU_DBD_DRIVER_FMT is defined in apu.h */
-    apr_snprintf(path, sizeof path, APU_DBD_DRIVER_FMT, name);
+
+#ifdef WIN32
+    apr_snprintf(path, sizeof path, "apr_dbd_%s.dll", name);
+#elif defined(NETWARE)
+    apr_snprintf(path, sizeof path, "dbd%s.nlm", name);
+#else
+    apr_snprintf(path, sizeof path, "%s/apr_dbd_%s.so", APU_DSO_LIBDIR, name);
+#endif
     rv = apr_dso_load(&dlhandle, path, pool);
     if (rv != APR_SUCCESS) { /* APR_EDSOOPEN */
         goto unlock;
     }
-    apr_snprintf(path, sizeof path, "apr_dbd_%s_driver", name);
+    sprintf(path, "apr_dbd_%s_driver", name);
     rv = apr_dso_sym(&symbol, dlhandle, path);
     if (rv != APR_SUCCESS) { /* APR_ESYMNOTFOUND */
         apr_dso_unload(dlhandle);
