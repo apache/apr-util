apache/apr-util

commit 096a38dfcb1c2436dceb75e8d8ab367934962e4d

Make apr_brigade_length() always update the length parameter. In case of a bucket read error, return the length up to the error.
PR: 40893


git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@558361 13f79535-47bb-0310-9956-ffa450edef68

buckets/apr_brigade.c
@@ -165,6 +165,7 @@ APU_DECLARE(apr_status_t) apr_brigade_length(apr_bucket_brigade *bb,
 {
     apr_off_t total = 0;
     apr_bucket *bkt;
+    apr_status_t status = APR_SUCCESS;
 
     for (bkt = APR_BRIGADE_FIRST(bb);
          bkt != APR_BRIGADE_SENTINEL(bb);
@@ -173,16 +174,15 @@ APU_DECLARE(apr_status_t) apr_brigade_length(apr_bucket_brigade *bb,
         if (bkt->length == (apr_size_t)(-1)) {
             const char *ignore;
             apr_size_t len;
-            apr_status_t status;
 
             if (!read_all) {
-                *length = -1;
-                return APR_SUCCESS;
+                total = -1;
+                break;
             }
 
             if ((status = apr_bucket_read(bkt, &ignore, &len,
                                           APR_BLOCK_READ)) != APR_SUCCESS) {
-                return status;
+                break;
             }
         }
 

include/apr_buckets.h
@@ -712,8 +712,9 @@ APU_DECLARE(apr_status_t) apr_brigade_partition(apr_bucket_brigade *b,
  * Return the total length of the brigade.
  * @param bb The brigade to compute the length of
  * @param read_all Read unknown-length buckets to force a size
- * @param length Returns the length of the brigade, or -1 if the brigade has
- *               buckets of indeterminate length and read_all is 0.
+ * @param length Returns the length of the brigade (up to the end, or up
+ *               to a bucket read error), or -1 if the brigade has buckets
+ *               of indeterminate length and read_all is 0.
  */
 APU_DECLARE(apr_status_t) apr_brigade_length(apr_bucket_brigade *bb,
                                              int read_all,

commit 9619c3057c4712468589d2183569aa7f9d57e1b2

Return the correct status. Fix bug introduced by r558361.
git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@558384 13f79535-47bb-0310-9956-ffa450edef68

buckets/apr_brigade.c
@@ -190,7 +190,7 @@ APU_DECLARE(apr_status_t) apr_brigade_length(apr_bucket_brigade *bb,
     }
 
     *length = total;
-    return APR_SUCCESS;
+    return status;
 }
 
 APU_DECLARE(apr_status_t) apr_brigade_flatten(apr_bucket_brigade *bb,