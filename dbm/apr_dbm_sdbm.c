e0fcf5981bfd54b8080c5e5064d45d6b9eb8daa2
Davi Arnaut <davi@apache.org>
2007-08-04 13:34:47 +0000

    The SDBM functions returns APR error codes.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@562718 13f79535-47bb-0310-9956-ffa450edef68

--- dbm/apr_dbm_sdbm.c
+++ dbm/apr_dbm_sdbm.c
@@ -34,19 +34,15 @@
 
 static apr_status_t set_error(apr_dbm_t *dbm, apr_status_t dbm_said)
 {
-    apr_status_t rv = APR_SUCCESS;
+    dbm->errcode = dbm_said;
 
-    /* ### ignore whatever the DBM said (dbm_said); ask it explicitly */
-
-    if ((dbm->errcode = dbm_said) == APR_SUCCESS) {
+    if (dbm_said != APR_SUCCESS) {
+        dbm->errmsg = apr_psprintf(dbm->pool, "%pm", &dbm_said);
+    } else {
         dbm->errmsg = NULL;
     }
-    else {
-        dbm->errmsg = "I/O error occurred.";
-        rv = APR_EGENERAL;        /* ### need something better */
-    }
 
-    return rv;
+    return dbm_said;
 }
 
 /* --------------------------------------------------------------------------
