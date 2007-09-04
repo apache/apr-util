e086a6bfa52b44725144ce6862bdd49727e7d704
William A. Rowe Jr <wrowe@apache.org>
2007-09-04 01:17:01 +0000

    Prepare for APR-util 1.2.10
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.2.x@572477 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -59,14 +59,14 @@
  * The Patch Level never includes API changes, simply bug fixes.
  * Reset to 0 when upgrading APR_MINOR_VERSION
  */
-#define APU_PATCH_VERSION       10
+#define APU_PATCH_VERSION      10
 
 /** 
  * The symbol APU_IS_DEV_VERSION is only defined for internal,
  * "development" copies of APU.  It is undefined for released versions
  * of APU.
  */
-#define APU_IS_DEV_VERSION
+/* #undef APU_IS_DEV_VERSION */
 
 
 #if defined(APU_IS_DEV_VERSION) || defined(DOXYGEN)
