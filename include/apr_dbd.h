daf207d6f86d0d3e58fcfe07324304f80ed78f9a
Bojan Smojver <bojan@apache.org>
2007-10-19 03:45:36 +0000

    Add group parameter to MySQL driver Call mysql_thread_init() explicitly, to avoid segfaults on some systems Patch by Ian Holsman
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@586248 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_dbd.h
+++ include/apr_dbd.h
@@ -125,13 +125,15 @@ APU_DECLARE(apr_status_t) apr_dbd_get_driver(apr_pool_t *pool, const char *name,
  *  keys, each followed by an equal sign and a value. Such key/value pairs can
  *  be delimited by space, CR, LF, tab, semicolon, vertical bar or comma.
  *  @remarks MySQL: the params can have "host", "port", "user", "pass",
- *  "dbname", "sock", "flags" and "fldsz" keys, each followed by an equal sign
- *  and a value. Such key/value pairs can be delimited by space, CR, LF, tab,
- *  semicolon, vertical bar or comma. For now, "flags" can only recognise
- *  CLIENT_FOUND_ROWS (check MySQL manual for details). The value associated
- *  with "fldsz" determines maximum amount of memory (in bytes) for each of
- *  the fields in the result set of prepared statements. By default, this
- *  value is 1 MB.
+ *  "dbname", "sock", "flags" "fldsz" and "group" keys, each followed by an
+ *  equal sign and a value. Such key/value pairs can be delimited by space,
+ *  CR, LF, tab, semicolon, vertical bar or comma. For now, "flags" can only
+ *  recognise CLIENT_FOUND_ROWS (check MySQL manual for details). The value
+ *  associated with "fldsz" determines maximum amount of memory (in bytes) for
+ *  each of the fields in the result set of prepared statements. By default,
+ *  this value is 1 MB. The value associated with "group" determines the which
+ *  group from configuration file to use (see MYSQL_READ_DEFAULT_GROUP option
+ *  of mysql_options() in MySQL manual).
  */
 APU_DECLARE(apr_status_t) apr_dbd_open(const apr_dbd_driver_t *driver,
                                        apr_pool_t *pool, const char *params,
