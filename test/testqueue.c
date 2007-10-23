c9028fcc92400139f35a7f38bfea0a3e2bdfbbb1
William A. Rowe Jr <wrowe@apache.org>
2007-10-23 21:18:42 +0000

    Fix some times.  testreslist remains mostly worthless
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.2.x@587656 13f79535-47bb-0310-9956-ffa450edef68

--- test/testqueue.c
+++ test/testqueue.c
@@ -28,7 +28,6 @@
 #define NUMBER_PRODUCERS    4
 #define PRODUCER_ACTIVITY   5
 #define QUEUE_SIZE          100
-#define SLEEP_TIME          30
 
 static apr_queue_t *queue;
 
@@ -117,7 +116,7 @@ static void test_queue_producer_consumer(abts_case *tc, void *data)
         ABTS_INT_EQUAL(tc, rv, APR_SUCCESS);
     }
 
-    apr_sleep(SLEEP_TIME * 1000000); /* sleep 10 seconds */
+    apr_sleep(5000000); /* sleep 5 seconds */
 
     rv = apr_queue_term(queue);
     ABTS_INT_EQUAL(tc, rv, APR_SUCCESS);
