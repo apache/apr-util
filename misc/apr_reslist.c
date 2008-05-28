2813236eb001d87ae41338edc505656540e3bab9
Nick Kew <niq@apache.org>
2008-05-28 21:45:13 +0000

    Backport r661063 - fix apr_reslist cleanup Folks on IRC want this backported in time for 1.3.0 roll.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.3.x@661102 13f79535-47bb-0310-9956-ffa450edef68

--- misc/apr_reslist.c
+++ misc/apr_reslist.c
@@ -137,18 +137,20 @@ static apr_status_t destroy_resource(apr_reslist_t *reslist, apr_res_t *res)
 
 static apr_status_t reslist_cleanup(void *data_)
 {
-    apr_status_t rv;
+    apr_status_t rv = APR_SUCCESS;
     apr_reslist_t *rl = data_;
     apr_res_t *res;
 
     apr_thread_mutex_lock(rl->listlock);
 
     while (rl->nidle > 0) {
+        apr_status_t rv1;
         res = pop_resource(rl);
         rl->ntotal--;
-        rv = destroy_resource(rl, res);
-        if (rv != APR_SUCCESS) {
-            return rv;
+        rv1 = destroy_resource(rl, res);
+        if (rv1 != APR_SUCCESS) {
+            rv = rv1;  /* loses info in the unlikely event of
+                        * multiple *different* failures */
         }
         free_container(rl, res);
     }
@@ -159,7 +161,7 @@ static apr_status_t reslist_cleanup(void *data_)
     apr_thread_mutex_destroy(rl->listlock);
     apr_thread_cond_destroy(rl->avail);
 
-    return APR_SUCCESS;
+    return rv;
 }
 
 /**
