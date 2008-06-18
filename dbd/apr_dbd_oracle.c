ae33e01e9cc780d21cacaa142a0c85633bb42ec7
Bojan Smojver <bojan@apache.org>
2008-06-18 02:39:22 +0000

    Backport r669055 from the trunk. Silence some compiler warnings on HP-UX.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.3.x@669058 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_oracle.c
+++ dbd/apr_dbd_oracle.c
@@ -407,9 +407,9 @@ static apr_dbd_t *dbd_oracle_open(apr_pool_t *pool, const char *params,
             ++ptr;
             continue;
         }
-        for (key = ptr-1; isspace(*key); --key);
+        for (key = ptr-1; apr_isspace(*key); --key);
         klen = 0;
-        while (isalpha(*key)) {
+        while (apr_isalpha(*key)) {
             if (key == params) {
                 /* Don't parse off the front of the params */
                 --key;
@@ -420,7 +420,7 @@ static apr_dbd_t *dbd_oracle_open(apr_pool_t *pool, const char *params,
             ++klen;
         }
         ++key;
-        for (value = ptr+1; isspace(*value); ++value);
+        for (value = ptr+1; apr_isspace(*value); ++value);
         vlen = strcspn(value, delims);
         for (i=0; fields[i].field != NULL; ++i) {
             if (!strncasecmp(fields[i].field, key, klen)) {
