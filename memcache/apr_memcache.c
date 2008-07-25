8dfb7069991d839936e9ed71c4153078873b1d3d
Bojan Smojver <bojan@apache.org>
2008-07-25 00:49:04 +0000

    Backport r679637 from the trunk. Fix parsing of rusage in stats data. PR 44587. Patch by Bryant Scott <bryantscott gmail.com>
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.3.x@679638 13f79535-47bb-0310-9956-ffa450edef68

--- memcache/apr_memcache.c
+++ memcache/apr_memcache.c
@@ -1517,15 +1517,11 @@ static apr_time_t stat_read_rtime(apr_pool_t *p, char *buf, apr_size_t  len)
     char *tok;
     char *secs;
     char *usecs;
-    const char *sep = ":";
+    const char *sep = ":.";
 
     buf[len-2] = '\0';
 
     secs = apr_strtok(buf, sep, &tok);
-    if (secs == NULL) {
-        sep = ".";
-        secs = apr_strtok(buf, sep, &tok);
-    }
     usecs = apr_strtok(NULL, sep, &tok);
     if (secs && usecs) {
         return apr_time_make(atoi(secs), atoi(usecs));
