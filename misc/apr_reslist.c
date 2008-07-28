599d0e79a906a865803b30b9a00fec98845d8c71
Bojan Smojver <bojan@apache.org>
2008-07-28 21:31:47 +0000

    Better enforce reslist limits. Values cannot be negative and hmax must not be zero.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/trunk@680511 13f79535-47bb-0310-9956-ffa450edef68

--- misc/apr_reslist.c
+++ misc/apr_reslist.c
@@ -245,7 +245,8 @@ APU_DECLARE(apr_status_t) apr_reslist_create(apr_reslist_t **reslist,
 
     /* Do some sanity checks so we don't thrash around in the
      * maintenance routine later. */
-    if (min > smax || min > hmax || smax > hmax || ttl < 0) {
+    if (min < 0 || min > smax || min > hmax || smax > hmax || hmax == 0 ||
+        ttl < 0) {
         return APR_EINVAL;
     }
 
