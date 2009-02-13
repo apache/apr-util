e27cb82d5c57c3fe4bf0d03c8fb539530570659c
Bojan Smojver <bojan@apache.org>
2009-02-13 05:03:10 +0000

    Do not crash if process is NULL. Do not print diagnostics to stderr (APU DBD operates silently). Do not allow driver to exit the process (always return INT_CANCEL on error).
    git-svn-id: https://svn.eu.apache.org/repos/asf/apr/apr-util/trunk@743986 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_freetds.c
+++ dbd/apr_dbd_freetds.c
@@ -606,9 +606,7 @@ static DBPROCESS *freetds_open(apr_pool_t *pool, const char *params,
 
     process = dbopen(login, server);
 
-    fprintf(stderr, "databaseName [%s]\n", databaseName);
-
-    if (databaseName != NULL)
+    if (process != NULL && databaseName != NULL)
     {
         dbuse(process, databaseName);
     }
@@ -691,6 +689,11 @@ static apr_status_t freetds_term(void *dummy)
     regfree(&dbd_freetds_find_arg);
     return APR_SUCCESS;
 }
+static int freetds_err_handler(DBPROCESS *dbproc, int severity, int dberr,
+                               int oserr, char *dberrstr, char *oserrstr)
+{
+    return INT_CANCEL; /* never exit */
+}
 static void dbd_freetds_init(apr_pool_t *pool)
 {
     int rv = regcomp(&dbd_freetds_find_arg,
@@ -701,6 +704,7 @@ static void dbd_freetds_init(apr_pool_t *pool)
         fprintf(stderr, "regcomp failed: %s\n", errmsg);
     }
     dbinit();
+    dberrhandle(freetds_err_handler);
     apr_pool_cleanup_register(pool, NULL, freetds_term, apr_pool_cleanup_null);
 }
 
