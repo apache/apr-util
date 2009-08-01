1907db7358520e330b624573821876d125771a21
William A. Rowe Jr <wrowe@apache.org>
2009-08-01 00:40:40 +0000

    Reorder the config into sanity, prefer include/mysql/ structure if found, and *be consistent*.
    On Win32, perhaps others, including my_* foo before mysql.h
    results in broken includes.
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.3.x@799780 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_mysql.c
+++ dbd/apr_dbd_mysql.c
@@ -15,7 +15,6 @@
  */
 
 #include "apu.h"
-#define HAVE_MYSQL_MYSQL_H
 
 #if APU_HAVE_MYSQL
 
@@ -25,22 +24,20 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-#ifdef HAVE_MY_GLOBAL_H
-#include <my_global.h>
-#elif defined(HAVE_MYSQL_MY_GLOBAL_H)
-#include <mysql/my_global.h>
-#endif
-#ifdef HAVE_MY_SYS_H
-#include <my_sys.h>
-#elif defined(HAVE_MYSQL_MY_SYS_H)
-#include <mysql/my_sys.h>
-#endif
-#ifdef HAVE_MYSQL_H
-#include <mysql.h>
-#include <errmsg.h>
-#elif defined(HAVE_MYSQL_MYSQL_H)
+#if defined(HAVE_MYSQL_MYSQL_H)
 #include <mysql/mysql.h>
 #include <mysql/errmsg.h>
+#if defined(HAVE_MYSQL_MY_GLOBAL_H) && defined(HAVE_MYSQL_MY_SYS_H)
+#include <mysql/my_global.h>
+#include <mysql/mysql.h>
+#endif
+#else
+#include <mysql.h>
+#include <errmsg.h>
+#if defined(HAVE_MY_GLOBAL_H) && defined(HAVE_MY_SYS_H)
+#include <my_global.h>
+#include <my_sys.h>
+#endif
 #endif
 
 #include "apr_strings.h"
