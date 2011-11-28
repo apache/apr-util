3f5e561e5551672463fb2eef7aeff299aab9912e
Graham Leggett <minfrin@apache.org>
2011-11-28 23:00:26 +0000

    Remove variables that we assign but never read.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.5.x@1207683 13f79535-47bb-0310-9956-ffa450edef68

--- crypto/apr_crypto_openssl.c
+++ crypto/apr_crypto_openssl.c
@@ -102,7 +102,6 @@ static apr_status_t crypto_shutdown(void)
 
 static apr_status_t crypto_shutdown_helper(void *data)
 {
-    apr_pool_t *pool = (apr_pool_t *) data;
     return crypto_shutdown();
 }
 
@@ -308,6 +307,7 @@ static apr_status_t crypto_get_block_key_types(apr_hash_t **types,
         const apr_crypto_t *f)
 {
     *types = f->types;
+    return APR_SUCCESS;
 }
 
 /**
@@ -322,6 +322,7 @@ static apr_status_t crypto_get_block_key_modes(apr_hash_t **modes,
         const apr_crypto_t *f)
 {
     *modes = f->modes;
+    return APR_SUCCESS;
 }
 
 /**
