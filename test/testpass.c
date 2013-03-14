5dd84cc480a63482ded376295608853f113051a7
Jeff Trawick <trawick@apache.org>
2013-03-14 13:10:54 +0000

    fix compile failure in r1449315
    (upper case version of apr_assert_success() isn't available in 1.5.x tests)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.5.x@1456414 13f79535-47bb-0310-9956-ffa450edef68

--- test/testpass.c
+++ test/testpass.c
@@ -167,7 +167,7 @@ static void test_glibc_shapass(abts_case *tc, void *data)
 {
     int i = 0;
     while (glibc_sha_pws[i].password) {
-        APR_ASSERT_SUCCESS(tc, "check for valid glibc crypt-sha password",
+        apr_assert_success(tc, "check for valid glibc crypt-sha password",
                            apr_password_validate(glibc_sha_pws[i].password,
                                                  glibc_sha_pws[i].hash));
         i++;
