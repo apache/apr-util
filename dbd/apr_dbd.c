8ec7cbd5d3a6cff725c3d7e281a2ed9fdb4027f9
Jeff Trawick <trawick@apache.org>
2013-10-06 20:08:37 +0000

    Merge r1529554 from trunk:
    APR DSOs have .dll extension on Cygwin too.
    
    PR: 55587
    Submitted by: Carlo Bramini <carlo.bramix libero.it>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.5.x@1529675 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd.c
+++ dbd/apr_dbd.c
@@ -186,7 +186,7 @@ APU_DECLARE(apr_status_t) apr_dbd_get_driver(apr_pool_t *pool, const char *name,
 
 #if defined(NETWARE)
     apr_snprintf(modname, sizeof(modname), "dbd%s.nlm", name);
-#elif defined(WIN32)
+#elif defined(WIN32) || defined(__CYGWIN__)
     apr_snprintf(modname, sizeof(modname),
                  "apr_dbd_%s-" APU_STRINGIFY(APU_MAJOR_VERSION) ".dll", name);
 #else
