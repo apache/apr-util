7babd2502866cfc95a5de4b634bad6b59c3dfdb1
Ivan Zhakov <ivan@apache.org>
2025-05-18 16:19:41 +0000

    Merge r1925677 from apr/trunk:   vcpkg: Add LDAP feature.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.7.x@1925678 13f79535-47bb-0310-9956-ffa450edef68

--- build/vcpkg/apr-util/portfile.cmake
+++ build/vcpkg/apr-util/portfile.cmake
@@ -18,6 +18,7 @@ if (VCPKG_TARGET_IS_WINDOWS)
             dbd-odbc FEATURE_DBD_ODBC
             dbd-sqlite3 FEATURE_DBD_SQLITE3
             dbd-postgresql FEATURE_DBD_PGQL
+            ldap FEATURE_LDAP
     )
 
     vcpkg_cmake_configure(
@@ -29,6 +30,7 @@ if (VCPKG_TARGET_IS_WINDOWS)
             -DAPU_HAVE_ODBC=${FEATURE_DBD_ODBC}
             -DAPU_HAVE_SQLITE3=${FEATURE_DBD_SQLITE3}
             -DAPU_HAVE_PGSQL=${FEATURE_DBD_PGQL}
+            -DAPR_HAS_LDAP=${FEATURE_LDAP}
             -DAPU_USE_EXPAT=ON
     )
 
