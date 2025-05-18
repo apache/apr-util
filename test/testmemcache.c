27b1c8e00cd631e71759e7e767caa78675a372f0
Ivan Zhakov <ivan@apache.org>
2025-05-18 12:40:35 +0000

    Merge r1910026 from apr/trunk:   Align the apr_memcache API with the apr_reslist API.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.7.x@1925666 13f79535-47bb-0310-9956-ffa450edef68

--- test/testmemcache.c
+++ test/testmemcache.c
@@ -241,7 +241,8 @@ static void test_memcache_create(abts_case * tc, void *data)
 
     port = PORT + i;
     rv =
-      apr_memcache_server_create(pool, HOST, PORT + i, 0, 1, 1, 60, &server);
+      apr_memcache_server_create(pool, HOST, PORT + i, 0, 1, 1,
+                                 apr_time_from_sec(60), &server);
     ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
     rv = apr_memcache_add_server(memcache, server);
@@ -263,7 +264,8 @@ static void test_memcache_create(abts_case * tc, void *data)
     ABTS_PTR_NOTNULL(tc, s);
   }
 
-  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1, 60, &server);
+  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1,
+                                  apr_time_from_sec(60), &server);
   ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
   rv = apr_memcache_add_server(memcache, server);
@@ -322,7 +324,8 @@ static void test_memcache_user_funcs(abts_case * tc, void *data)
   for(i = 1; i <= 10; i++) {
     apr_memcache_server_t *ms;
 
-    rv = apr_memcache_server_create(pool, HOST, i, 0, 1, 1, 60, &ms);
+    rv = apr_memcache_server_create(pool, HOST, i, 0, 1, 1,
+                                    apr_time_from_sec(60), &ms);
     ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
     rv = apr_memcache_add_server(memcache, ms);
@@ -358,7 +361,8 @@ static void test_memcache_meta(abts_case * tc, void *data)
     rv = apr_memcache_create(pool, 1, 0, &memcache);
     ABTS_ASSERT(tc, "memcache create failed", rv == APR_SUCCESS);
 
-    rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1, 60, &server);
+    rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1,
+                                    apr_time_from_sec(60), &server);
     ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
     rv = apr_memcache_add_server(memcache, server);
@@ -427,7 +431,8 @@ static void test_memcache_addreplace(abts_case * tc, void *data)
   rv = apr_memcache_create(pool, 1, 0, &memcache);
   ABTS_ASSERT(tc, "memcache create failed", rv == APR_SUCCESS);
 
-  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1, 60, &server);
+  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1,
+                                  apr_time_from_sec(60), &server);
   ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
   rv = apr_memcache_add_server(memcache, server);
@@ -491,7 +496,8 @@ static void test_memcache_incrdecr(abts_case * tc, void *data)
   rv = apr_memcache_create(pool, 1, 0, &memcache);
   ABTS_ASSERT(tc, "memcache create failed", rv == APR_SUCCESS);
 
-  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1, 60, &server);
+  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1,
+                                  apr_time_from_sec(60), &server);
   ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
   rv = apr_memcache_add_server(memcache, server);
@@ -548,7 +554,8 @@ static void test_memcache_multiget(abts_case * tc, void *data)
   rv = apr_memcache_create(pool, 1, 0, &memcache);
   ABTS_ASSERT(tc, "memcache create failed", rv == APR_SUCCESS);
 
-  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1, 60, &server);
+  rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1,
+                                  apr_time_from_sec(60), &server);
   ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
   rv = apr_memcache_add_server(memcache, server);
@@ -621,7 +628,8 @@ static void test_memcache_setget(abts_case * tc, void *data)
     rv = apr_memcache_create(pool, 1, 0, &memcache);
     ABTS_ASSERT(tc, "memcache create failed", rv == APR_SUCCESS);
 
-    rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1, 60, &server);
+    rv = apr_memcache_server_create(pool, HOST, PORT, 0, 1, 1,
+                                    apr_time_from_sec(60), &server);
     ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
     rv = apr_memcache_add_server(memcache, server);
@@ -714,7 +722,8 @@ static void test_connection_validation(abts_case *tc, void *data)
     rv = apr_memcache_create(p, 1, 0, &memcache);
     ABTS_ASSERT(tc, "memcache create failed", rv == APR_SUCCESS);
 
-    rv = apr_memcache_server_create(p, MOCK_HOST, MOCK_PORT, 0, 1, 1, 60000, &memserver);
+    rv = apr_memcache_server_create(p, MOCK_HOST, MOCK_PORT, 0, 1, 1,
+                                    apr_time_from_sec(60), &memserver);
     ABTS_ASSERT(tc, "server create failed", rv == APR_SUCCESS);
 
     rv = apr_memcache_add_server(memcache, memserver);
