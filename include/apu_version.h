9c3c376d0f02d36a378e256be90a71d5f7656456
Eric Covener <covener@apache.org>
2026-08-06 12:43:27 +0000

    publishing release apr-util-1.6.4
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr-util/branches/1.6.x@1936914 13f79535-47bb-0310-9956-ffa450edef68

--- include/apu_version.h
+++ include/apu_version.h
@@ -61,7 +61,7 @@
  * The Patch Level never includes API changes, simply bug fixes.
  * Reset to 0 when upgrading APR_MINOR_VERSION
  */
-#define APU_PATCH_VERSION       4
+#define APU_PATCH_VERSION       5
 
 /** 
  * The symbol APU_IS_DEV_VERSION is only defined for internal,
